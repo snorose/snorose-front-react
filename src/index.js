f341d412431ed52e7ee43d524889377a180f6122
haesa <xdhaesa@gmail.com>
2024-06-25 12:46:35 +0900

    [CHORE] prettier 초기 설정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode fri>
     <App />
   </React.StrictMode>
 );
