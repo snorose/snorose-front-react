aa6d5f0df90f956af46d8459778bfde6eed2f3bc
haesa <xdhaesa@gmail.com>
2024-07-01 00:07:47 +0900

    #9 [FEAT] Navbar 메뉴 클릭 시 선택된 메뉴의 아이콘 활성화
    배열 map 함수 내부의 Link에 누락된 key 추가

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { MENUS } from '../../constants';
 import Icon from '../Icon/Icon';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const { pathname } = useLocation();
+  const currentMenu = pathname.split('/')[1];
+  const getCurrentMenuColor = (id, pointColor, defaultColor) =>
+    id === currentMenu ? pointColor : defaultColor;
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.menus}>
         {MENUS &&
-          MENUS.map(({ id, label, ...props }) => (
-            <Link to={`/${id}`}>
+          MENUS.map(({ id, label, fill, ...props }) => (
+            <Link key={id} to={`/${id}`}>
               <li className={styles.menu}>
-                <Icon id={id} {...props} />
-                <span>{label}</span>
+                <Icon
+                  id={id}
+                  fill={getCurrentMenuColor(id, '#00368E', '#BFD7EC')}
+                  {...props}
+                />
+                <span
+                  style={{
+                    color: getCurrentMenuColor(id, '#00368E', '#5F86BF'),
+                  }}
+                >
+                  {label}
+                </span>
               </li>
             </Link>
           ))}
