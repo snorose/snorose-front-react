1e73d71314725d83af2df9f3559e2311e7816fa1
haesa <xdhaesa@gmail.com>
2024-07-01 19:01:41 +0900

    #14 [STYLE] ErrorPage 화면 수정
    HTTP 에러에 맞는 이미지와 에러 메시지를 보여주도록 수정

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
+import { ERRORS } from '../../constants';
 import styles from './ErrorPage.module.css';
 
 export default function ErrorPage() {
   const error = useRouteError();
 
+  const CURRENT_ERROR_DATA = ERRORS.filter(
+    (ERROR) => ERROR.status === error.status
+  )[0];
+  const imagePath = require(`../../assets/images/error${error.status}.png`);
+
   return (
     <main className={styles.errorPage}>
-      <p className={styles.title}>Oops!</p>
-      <p className={styles.description}>
-        Sorry, an unexpected error has occurred.
-      </p>
-      <p className={styles.errorStatus}>{error.status}</p>
-      <p>
-        <i className={styles.errorMessage}>
-          {error.statusText || error.message}
-        </i>
-      </p>
+      <img src={imagePath} alt={`${error.status} error`} />
+      <p className={styles.title}>{CURRENT_ERROR_DATA.messages[0]}</p>
+      {CURRENT_ERROR_DATA.messages.length > 1 && (
+        <p className={styles.text}>{CURRENT_ERROR_DATA.messages[1]}</p>
+      )}
     </main>
   );
 }
