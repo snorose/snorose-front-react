48931a55a5eef593bc9d3d2a871322125c8e146d
haesa <xdhaesa@gmail.com>
2024-07-02 02:14:11 +0900

    #16 [FEAT] Sidebar에서 메뉴 클릭 시 해당 메뉴로 이동
    라우트 연결

---
+++ src/pages/BoardDetailPage/BoardDetailPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function BoardDetailPage() {
+  const { pathname } = useLocation();
+  const currentPath = pathname.split('/')[2];
+  let currentBoard = '';
+
+  if (currentPath === 'first-snow') {
+    currentBoard = '첫눈온방';
+  } else if (currentPath === 'large-snow') {
+    currentBoard = '함박눈방';
+  } else if (currentPath === 'permanent-snow') {
+    currentBoard = '만년설방';
+  } else if (currentPath === 'besookt') {
+    currentBoard = '베숙트';
+  }
+
+  return <div>{currentBoard}</div>;
+}
