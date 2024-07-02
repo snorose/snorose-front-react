4d2c8ac1c7060a56cd1b44ca0d685e5d90e42924
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-03 00:04:28 +0900

    #11 [FEAT] 게시판 페이지 추가

---
+++ src/pages/BoardListPage/BoardListPage.jsx
@@ -0,0 +1,30 @@
+import styles from './BoardListPage.module.css';
+import Icon from '../../components/Icon/Icon.jsx';
+import sponser from '../../assets/sponser.svg';
+
+export default function BoardListPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.side_menu_btn}>
+          <Icon id='hamburger' />
+        </div>
+        <h1>게시판 타이틀</h1>
+      </div>
+      <div className={styles.notification_bar}>
+        <div className={styles.notification_icon}>
+          <Icon id='notification' />
+        </div>
+        <p>필독 공지사항</p>
+      </div>
+      <div className={styles.keyword_box}>
+        <div className={styles.keyword}>꿀팁</div>
+        <div className={styles.keyword}>일상</div>
+        <div className={styles.keyword}>우리과 찾기</div>
+      </div>
+      <div className={styles.sponser}>
+        <img src={sponser} alt='sponser' />
+      </div>
+    </div>
+  );
+}
