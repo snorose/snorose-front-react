bd885fb33b9bdffe5cdf86893add48ce8cc16a74
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-02 18:48:40 +0900

    #11 [REFACTOR] dev 기준으로 코드 스타일 수정

--- src/constants/menus.js
+++ src/constants/menus.js
@@ -35,3 +35,26 @@ export const MENUS = Object.freeze([
     height: 16,
   },
 ]);
+
+export const BOARDMENUS = [
+  {
+    id: 'board-cloud',
+    title: '첫눈온방',
+    desc: '눈송이 모두가 이용하는 커뮤니티',
+  },
+  {
+    id: 'board-snowy',
+    title: '함박눈방',
+    desc: '눈송이 모두가 이용하는 커뮤니티',
+  },
+  {
+    id: 'board-sunny',
+    title: '만년설방',
+    desc: '졸업생 전용 게시판',
+  },
+  {
+    id: 'board-star',
+    title: '베숙트',
+    desc: '추천을 가장 많이 받은 게시물 모아보기',
+  },
+];
--- src/pages/BoardPage/BoardPage.jsx
+++ src/pages/BoardPage/BoardPage.jsx
@@ -1,5 +1,50 @@
-import React from 'react';
+import styles from './BoardPage.module.css';
+import Icon from '../../components/Icon/Icon.jsx';
+import sponser from '../../assets/sponser.svg';
+import { BOARDMENUS } from '../../constants/menus.js';
+
+const BoardBar = ({ data }) => {
+  return (
+    <div className={styles.board_list_bar}>
+      <div className={styles.board_bar_icon}>
+        <Icon id={data.id} />
+      </div>
+      <div className={styles.board_bar_title}>{data.title}</div>
+      <div className={styles.board_bar_description}>{data.desc}</div>
+    </div>
+  );
+};
 
 export default function BoardPage() {
-  return <main>게시판</main>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.side_menu_btn}>
+          <Icon id='hamburger' />
+        </div>
+      </div>
+      <div className={styles.sponser}>
+        <img src={sponser} alt='sponser' />
+      </div>
+      <div className={styles.searchbar_box}>
+        <div className={styles.searchbar}>
+          <input type='text' placeholder='전체 게시판 검색' />
+          <div>
+            <Icon id='search' />
+          </div>
+        </div>
+      </div>
+      <div className={styles.board_box}>
+        <div className={styles.board_title}>커뮤니티</div>
+        <div className={styles.board_list_box}>
+          {BOARDMENUS.map((board, index) => (
+            <BoardBar key={index} data={board} />
+          ))}
+        </div>
+      </div>
+      <div className={styles.more_board_box}>
+        스노로즈에서 더 다양한 게시판을 준비하고 있어요
+      </div>
+    </div>
+  );
 }
