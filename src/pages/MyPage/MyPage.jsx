8f97f07775156ab5d60d0e591ac46fea56568f35
misung.dev <misung.dev@gmail.com>
2024-07-05 14:24:31 +0900

    #18 [FEAT] 마이페이지 상단 구현

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styles from './MyPage.module.css';
+import profileIcon from '../../assets/icon-profile.svg';
 
 export default function MyPage() {
-  return <main>마이페이지</main>;
+  return (
+    <main className={styles.myPage}>
+      <div className={styles.myPageUpper}>
+        <div className={styles.logoOverlay}></div>
+        <div className={styles.myInfo}>
+          <p className={styles.name}>힘하리</p>
+          <div className={styles.myImg}>
+            <img src={profileIcon} alt='프로필 이미지' />
+          </div>
+          <div className={styles.profileYearMember}>
+            <p className={styles.yearOfAdmission}>17학번</p>
+            <p>|</p>
+            <p className={styles.memberType}>정회원</p>
+          </div>
+        </div>
+      </div>
+      <div>이하 정보</div>
+    </main>
+  );
 }
