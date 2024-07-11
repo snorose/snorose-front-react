0ee72e948b21f8d0c37777ad47e93bd3cd562849
haesa <xdhaesa@gmail.com>
2024-07-12 03:26:11 +0900

    #30 [FEAT] Carousel 구현

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,37 @@
+import { useRef, useEffect, useState } from 'react';
+import styles from './Carousel.module.css';
+
+export default function Carousel({ children }) {
+  const carouselRef = useRef();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const carouselTimer = setInterval(() => {
+      if (currentIndex + 1 === children.length) {
+        setCurrentIndex(0);
+        carouselRef.current.style.transform = `translateX(0)`;
+      } else {
+        setCurrentIndex((prev) => prev + 1);
+        carouselRef.current.style.transform = `translateX(calc(-1 * (100vw - 14px) * ${currentIndex + 1}))`;
+      }
+    }, 5000);
+
+    return () => clearInterval(carouselTimer);
+  }, [children.length, currentIndex]);
+
+  return (
+    <div>
+      <div ref={carouselRef} className={styles.carousel}>
+        {children}
+      </div>
+      <div className={styles.indicator}>
+        {Array.from({ length: children.length }).map((_, index) => (
+          <span
+            key={index}
+            className={`${styles.dot} ${index === currentIndex && styles.current}`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-
 import Header from '../../components/Header/Header';
+import Carousel from '../../components/Carousel/Carousel.jsx';
+import Slide from '../../components/Carousel/Slide.jsx';
+import BANNER1 from '../../assets/images/ad_banner1.png';
+import BANNER2 from '../../assets/images/ad_banner2.png';
+import BANNER3 from '../../assets/images/ad_banner3.png';
 
 export default function MainPage() {
   return (
     <main>
       <Header />
+      <Carousel>
+        <Slide src={BANNER1} alt='banner1' />
+        <Slide src={BANNER2} alt='banner2' />
+        <Slide src={BANNER3} alt='banner3' />
+      </Carousel>
       메인홈
     </main>
   );
