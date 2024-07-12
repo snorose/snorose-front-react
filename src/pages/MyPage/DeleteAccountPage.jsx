15257c2c625392f945e4601ee97dac3a639060fb
misung.dev <misung.dev@gmail.com>
2024-07-12 19:49:20 +0900

    #33 [FEAT] 회원 탈퇴 페이지 생성 및 마이페이지 너비 수정

---
+++ src/pages/MyPage/DeleteAccountPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './DeleteAccountPage.module.css';
+
+export default function DeleteAccountPage() {
+  return <main className={styles.DeleteAccountPage}></main>;
+}
