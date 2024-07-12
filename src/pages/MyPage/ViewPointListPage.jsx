54430a725967087491766716844b91f3309af450
misung.dev <misung.dev@gmail.com>
2024-07-12 21:15:03 +0900

    #33 [FEAT] 포인트 내역 보기 페이지 생성

---
+++ src/pages/MyPage/ViewPointListPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './ViewPointListPage.module.css';
+
+export default function ViewPointListPage() {
+  return <main className={styles.viewPointListPage}>포인트 내역 보기</main>;
+}
