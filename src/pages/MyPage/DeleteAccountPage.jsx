c6cdf45808860dbb26a60410fc1a9c85845a76be
misung.dev <misung.dev@gmail.com>
2024-07-12 20:30:56 +0900

    #33 [REFACTOR] 회원 탈퇴 페이지 리팩토링

--- src/pages/MyPage/DeleteAccountPage.jsx
+++ src/pages/MyPage/DeleteAccountPage.jsx
@@ -3,6 +3,12 @@ import styles from './DeleteAccountPage.module.css';
 import { Link } from 'react-router-dom';
 import Icon from '../../components/Icon/Icon.jsx';
 
+const descriptions = [
+  '• 아이디, 이메일, 학번은 .. 목적으로 6개월간 보관됩니다',
+  '• 보유 포인트 및 포인트 기록은 복구가 불가능합니다',
+  '• 등등..',
+];
+
 export default function DeleteAccountPage() {
   return (
     <main className={styles.DeleteAccountPage}>
@@ -13,13 +19,11 @@ export default function DeleteAccountPage() {
       </div>
       <h1 className={styles.title}>탈퇴 시 아래 내용을 확인해주세요</h1>
       <div className={styles.descWrapper}>
-        <p className={styles.desc}>
-          • 아이디, 이메일, 학번은 .. 목적으로 6개월간 보관됩니다
-        </p>
-        <p className={styles.desc}>
-          • 보유 포인트 및 포인트 기록은 복구가 불가능합니다
-        </p>
-        <p className={styles.desc}>• 등등..</p>
+        {descriptions.map((desc, index) => (
+          <p key={index} className={styles.desc}>
+            {desc}
+          </p>
+        ))}
       </div>
       <div className={styles.buttonWrapper}>
         <Link to='/my-page' className={styles.goBackButton}>
