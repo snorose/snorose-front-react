6b0f9fa520ac92a0d7348add9bf7ee82e3aa2a8b
misung.dev <misung.dev@gmail.com>
2024-07-12 21:55:07 +0900

    #33 [FEAT] 포인트 내역 보기 페이지 일부 구현

--- src/pages/MyPage/ViewPointListPage.jsx
+++ src/pages/MyPage/ViewPointListPage.jsx
@@ -24,6 +24,37 @@ export default function ViewPointListPage() {
               <Icon id='point-plus' className={styles.icon} />
             </div>
             <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>출석 포인트</div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>+ 3</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-plus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>게시글 포인트</div>
+              <div className={styles.pointDesc}>뫔뫄</div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>+ 3</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-plus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
               <div className={styles.pointTitle}>댓글 포인트</div>
               <div className={styles.pointDesc}>
                 어제 간식으로 나온거 맛있던데 총학 공지에 나와있는...
@@ -33,6 +64,86 @@ export default function ViewPointListPage() {
           </div>
           <div className={styles.pointScore}>
             <p className={styles.chargePoint}>+ 2</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-minus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>시험후기 다운로드</div>
+              <div className={styles.pointDesc}>서양미술사/이교수</div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>- 50</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-plus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>인증 포인트</div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>+ 50</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-plus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>신고 포상 포인트</div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>+ 10</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-minus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>게시글 삭제 포인트</div>
+              <div className={styles.pointDesc}>아미친 지각 각임</div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>- 3</p>
+          </div>
+        </div>
+
+        <div className={styles.pointBox}>
+          <div className={styles.pointWrapper}>
+            <div className={styles.pointIconWrapper}>
+              <Icon id='point-minus' className={styles.icon} />
+            </div>
+            <div className={styles.pointContent}>
+              <div className={styles.pointTitle}>댓글 삭제 포인트</div>
+              <div className={styles.pointDesc}>
+                어제 간식으로 나온거 맛있던데 총학 공지에 나와있는...
+              </div>
+              <div className={styles.pointDate}>2024.06.01 15:23:00</div>
+            </div>
+          </div>
+          <div className={styles.pointScore}>
+            <p className={styles.chargePoint}>- 3</p>
           </div>
         </div>
       </div>
