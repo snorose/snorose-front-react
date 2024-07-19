74b63ab34f20c62249ce58245c99fec30262c6c4
haesa <xdhaesa@gmail.com>
2024-07-19 18:29:57 +0900

    #30 [ADD] role 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,13 @@
 import { BOARDMENUS } from './boardmenus.js';
 import { ERRORS } from './errors';
 import { NAVBAR_MENUS, NOT_LOGIN_MENUS, SIDEBAR_MENUS } from './menus';
+import { ROLE } from './role.js';
 
-export { BOARDMENUS, ERRORS, NAVBAR_MENUS, NOT_LOGIN_MENUS, SIDEBAR_MENUS };
+export {
+  BOARDMENUS,
+  ERRORS,
+  NAVBAR_MENUS,
+  NOT_LOGIN_MENUS,
+  ROLE,
+  SIDEBAR_MENUS,
+};
---
+++ src/constants/role.js
@@ -0,0 +1,7 @@
+export const ROLE = Object.freeze({
+  preUser: 1,
+  User: 2,
+  User2: 3,
+  Admin: 4,
+  Official: 5,
+});
