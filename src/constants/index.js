04d92420e753d0bf79e94b4ea7fd296759ac7923
haesa <xdhaesa@gmail.com>
2024-07-19 15:20:38 +0900

    #30 [ADD] 보드 메뉴 데이터 constants/index.js에 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
+import { BOARDMENUS } from './boardmenus.js';
 import { ERRORS } from './errors';
 import { NAVBAR_MENUS, NOT_LOGIN_MENUS, SIDEBAR_MENUS } from './menus';
 
-export { ERRORS, NAVBAR_MENUS, NOT_LOGIN_MENUS, SIDEBAR_MENUS };
+export { BOARDMENUS, ERRORS, NAVBAR_MENUS, NOT_LOGIN_MENUS, SIDEBAR_MENUS };
