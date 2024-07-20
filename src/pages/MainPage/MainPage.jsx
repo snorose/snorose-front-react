96dbc62a3ed1a21daa5ccb23439cc3b6a712c442
haesa <xdhaesa@gmail.com>
2024-07-20 14:45:34 +0900

    #30 [STYLE] 메인페이지 공지사항 UI 컴포넌트를 Card로 교체

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
+import Card from '../../components/Card/Card.jsx';
 import Carousel from '../../components/Carousel/Carousel.jsx';
 import CategoryBoard from '../../components/CategoryBoard/CategoryBoard.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
 import Header from '../../components/Header/Header';
-import Notice from '../../components/Notice/Notice.jsx';
+import Margin from '../../components/Margin/Margin.jsx';
 import Slide from '../../components/Carousel/Slide.jsx';
-import { BESOOKTS, CAROUSEL_BANNER } from '../../dummy/data';
+import { BESOOKTS, CAROUSEL_BANNER, USER } from '../../dummy/data';
 import { BOARDMENUS } from '../../constants/boardmenus.js';
+import styles from './MainPage.module.css';
 
 const BOARDS = BOARDMENUS.filter((board) => board.title !== '베숙트');
 
@@ -14,11 +16,32 @@ export default function MainPage() {
     <main>
       <Header />
       <Carousel>
-        {CAROUSEL_BANNER.map((banner) => (
-          <Slide src={banner} alt='banner' />
+        {CAROUSEL_BANNER.map((banner, index) => (
+          <Slide key={index} src={banner} alt='banner' />
         ))}
       </Carousel>
-      <Notice />
+      <Margin className={styles.cards}>
+        <Card
+          className={styles.notice}
+          to='/notice'
+          title='시험후기 등록 기간 공지'
+          tag='공지'
+          imgPath='megaphone.svg'
+          icon={{ id: 'megaphone', width: 128, height: 131 }}
+          isDark={USER?.isLogin ? false : true}
+        />
+        {USER?.isLogin && (
+          <Card
+            className={styles.mark}
+            to='/'
+            title='오늘의 출석체크'
+            tag='출석체크'
+            imgPath='flag.svg'
+            icon={{ id: 'flag', width: 85, height: 101 }}
+            isDark
+          />
+        )}
+      </Margin>
       <CategoryBoard title='카테고리' to='/board'>
         {BOARDS.map(({ id, title, desc }) => (
           <CategoryBoard.Item
