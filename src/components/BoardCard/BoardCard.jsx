828e5eaf5981d1fc17276538701ce1b35367f67a
haesa <xdhaesa@gmail.com>
2024-07-20 17:36:17 +0900

    #30 [FEAT] BoardCard 컴포넌트에 로그인 상태에 따른 조건부 렌더링 추가

--- src/components/BoardCard/BoardCard.jsx
+++ src/components/BoardCard/BoardCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { USER } from '../../dummy/data';
 import styles from './BoardCard.module.css';
 
 export default function BoardCard({
@@ -15,11 +16,15 @@ export default function BoardCard({
       <div
         className={styles.card}
         style={{
-          backgroundImage: `url(${path})`,
+          backgroundImage: `${USER?.isLogin ? `url(${path})` : ''}`,
         }}
       >
-        <p className={styles.name}>{name}</p>
-        <p className={styles.desc}>{desc}</p>
+        <p className={`${styles.name} ${!USER?.isLogin && styles.notLogin}`}>
+          {USER?.isLogin ? name : '로그인하세요'}
+        </p>
+        <p className={styles.desc}>
+          {USER?.isLogin ? desc : `로그인 후\n이용 가능합니다`}
+        </p>
       </div>
     </Link>
   );
