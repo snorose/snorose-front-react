59560d0819d91cbfb59cff7a2ee2f8798276a716
haesa <xdhaesa@gmail.com>
2024-07-21 00:15:56 +0900

    #30 [STYLE] 새로운 디자인으로 Header 컴포넌트 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,26 +2,26 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Icon from '../../components/Icon/Icon';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import { USER } from '../../dummy/data';
 import styles from './Header.module.css';
 
-const isLogin = false;
-
-export default function Header() {
+export default function Header({ className }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <header className={styles.header}>
+      <header className={`${styles.header} ${className}`}>
+        <Icon id='logo' width={151} height={27} />
         <Icon
           id='hamburger'
-          width={17}
-          height={17}
+          width={23}
+          height={16}
           onClick={(event) => {
             event.stopPropagation();
             setIsOpen((prev) => !prev);
           }}
         />
-        {!isLogin && (
+        {!USER?.isLogin && (
           <Link to='/login' className={styles.login}>
             Login
           </Link>
