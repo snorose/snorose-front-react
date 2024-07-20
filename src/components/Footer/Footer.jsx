bbfaae4bad2797c507c767a495763c2fd0d63711
haesa <xdhaesa@gmail.com>
2024-07-20 20:01:53 +0900

    #30 [STYLE] 새로 디자인된 Footer로 교체

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,14 +4,16 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.title}>SNOROSE</p>
-      <p>CONTACT US : smsnorose@gmail.com</p>
-      <p>숙명여대 후원하기 : 신한 110-123-123123</p>
-      <p className={styles.info}>
-        <span>이용 약관</span>|<span>배너 및 광고 문의</span>|
-        <span>개인정보 처리방침</span>|
-        <Icon id='insta' width={11} height={11} />
-      </p>
+      <Icon id='logo' width={109} height={20} />
+      <div className={styles.info}>
+        <p className={styles.contact}>CONTACT US : smsnorose@gmail.com</p>
+        <p className={styles.give}>숙명여대 후원하기 : 신한 110-123-123123</p>
+        <div className={styles.menu}>
+          <span>이용 약관</span>|<span>배너 및 광고 문의</span>|
+          <span>개인정보 처리방침</span>|
+          <Icon id='insta' width={16.5} height={16.5} />
+        </div>
+      </div>
     </footer>
   );
 }
