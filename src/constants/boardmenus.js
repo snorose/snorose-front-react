c608903ddc7e9ca362f53a7fecfed4874e09345c
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-21 16:54:48 +0900

    #38 게시판 페이지 - 변경 된 디자인으로 스타일링 수정

--- src/constants/boardmenus.js
+++ src/constants/boardmenus.js
@@ -2,12 +2,12 @@ export const BOARD_MENUS = [
   {
     id: 'first-snow',
     title: '첫눈온방',
-    desc: '눈송이 모두가 이용하는 커뮤니티',
+    desc: '새내기 전용 커뮤니티',
   },
   {
     id: 'large-snow',
     title: '함박눈방',
-    desc: '눈송이 모두가 이용하는 커뮤니티',
+    desc: '눈송이 모두가\n이용하는 커뮤니티',
   },
   {
     id: 'permanent-snow',
@@ -17,6 +17,6 @@ export const BOARD_MENUS = [
   {
     id: 'besookt',
     title: '베숙트',
-    desc: '추천을 가장 많이 받은 게시물 모아보기',
+    desc: '추천을 가장 많이\n받은 게시물 모아보기',
   },
 ];
