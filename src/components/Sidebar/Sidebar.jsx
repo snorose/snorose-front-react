b59781ea52d0e6990d48d702bda800934226e92b
haesa <xdhaesa@gmail.com>
2024-07-26 19:31:36 +0900

    #44 [STYLE] Sidebar 컴포넌트에 새로운 디자인 적용

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Icon from '../Icon/Icon';
 import { NOT_LOGIN_MENUS, SIDEBAR_MENUS } from '../../constants';
+import { USER } from '../../dummy/data';
 import List from './List/List';
 import styles from './Sidebar.module.css';
 
-const isLogin = true;
-
 export default function Sidebar({ isOpen, setIsOpen }) {
-  const [FIRST_MENU, ...MENUS] = isLogin
+  const MENUS = USER.isLogin
     ? SIDEBAR_MENUS
     : SIDEBAR_MENUS.filter((menu) => NOT_LOGIN_MENUS.includes(menu.title));
   const handleEventPropagation = (event) => {
@@ -31,20 +31,21 @@ export default function Sidebar({ isOpen, setIsOpen }) {
   }
 
   return (
-    <aside onClick={handleEventPropagation} className={styles.sidebar}>
-      <Link to={FIRST_MENU.to}>
-        <h1 className={`${styles.title} ${styles.logo}`}>{FIRST_MENU.title}</h1>
-      </Link>
-      <List items={FIRST_MENU.items} />
-      {MENUS &&
-        MENUS.map(({ to, title, items }) => (
-          <div key={title}>
-            <Link to={to}>
-              <h3 className={styles.title}>{title}</h3>
-            </Link>
-            <List items={items} />
-          </div>
-        ))}
-    </aside>
+    <div className={styles.dim}>
+      <aside onClick={handleEventPropagation} className={styles.sidebar}>
+        <Link className={styles.logo} to='/'>
+          <Icon id='logo' width={129} height={23} margin={10} />
+        </Link>
+        {MENUS &&
+          MENUS.map(({ to, title, items }) => (
+            <div key={title}>
+              <Link to={to}>
+                <h3 className={styles.title}>{title}</h3>
+              </Link>
+              <List className={styles.item} items={items} />
+            </div>
+          ))}
+      </aside>
+    </div>
   );
 }
