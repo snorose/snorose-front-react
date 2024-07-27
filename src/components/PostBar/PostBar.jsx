18c20a3131fbc32d6cd5332dc5ef1f52f30eea42
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-27 18:37:41 +0900

    [FEAT] #48 게시판 리스트 페이지 - 새로운 디자인으로 수정

--- src/components/PostBar/PostBar.jsx
+++ src/components/PostBar/PostBar.jsx
@@ -2,36 +2,30 @@ import styles from './PostBar.module.css';
 import Icon from '../../components/Icon/Icon.jsx';
 import timeAgo from '../../utils/timeAgo.js';
 
-
 export default function PostBar({ data }) {
   const givenTime = data.createdAt;
   const agoTime = timeAgo(givenTime);
-  
+
   return (
     <div className={styles.post}>
       <div className={styles.post_top}>
-        <div className={styles.profile}>
-          <Icon id='profile' />
-        </div>
+        <Icon id='profile' width={15} height={15} />
         <p className={styles.name}>{data.userDisplay}</p>
-        <p className={styles.time}>{agoTime}</p>
-        {data.notice ? <p className={styles.edited}>(수정됨)</p> : null}
+        <p className={styles.dot}>·</p>
+        <p>{agoTime}</p>
+        {data.notice ? <p className={styles.edited}>&nbsp;(수정됨)</p> : null}
         <div className={styles.more_option}>
-          <Icon id='more-option' />
+          <Icon id='more-option' width={3} height={11} fill='#484848' />
         </div>
       </div>
       <div className={styles.post_center}>
         <p className={styles.title}>{data.title}</p>
-        <p className={styles.text}>post_id로 텍스트 받아와서 표시?</p>
+        <p className={styles.text}>post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서 post_id로 텍스트 받아와서</p>
       </div>
       <div className={styles.post_bottom}>
-        <div className={styles.comment_icon}>
-          <Icon id='comment' />
-        </div>
+        <Icon id='comment' width={12} height={10} fill='#D9D9D9' />
         <p className={styles.comment_cnt}>{data.viewCount}</p>
-        <div className={styles.like_icon}>
-          <Icon id='blank-heart' />
-        </div>
+        <Icon id='blank-heart' width={11} height={10} fill='#D9D9D9' />
         <p className={styles.like_cnt}>{data.likeCount}</p>
       </div>
     </div>
