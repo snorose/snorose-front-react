49a2b0d85d6f1684c4a331b0df6c55b5c6861a07
haesa <xdhaesa@gmail.com>
2024-07-29 22:01:04 +0900

    #60 [REFACTOR] Header에서 사이드바를 제어하는 메뉴 아이콘을 MenuIcon으로 분리

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Icon from '../../components/Icon/Icon';
-import Sidebar from '../../components/Sidebar/Sidebar';
 import styles from './Header.module.css';
+import MenuIcon from '../MenuIcon/MenuIcon.jsx';
 
 export default function Header({ className }) {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <header className={`${styles.header} ${className}`}>
         <Icon id='logo' width={151} height={27} />
-        <Icon
-          id='hamburger'
-          width={23}
-          height={16}
-          onClick={(event) => {
-            event.stopPropagation();
-            setIsOpen((prev) => !prev);
-          }}
-        />
+        <MenuIcon />
       </header>
-      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 }
