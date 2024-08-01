04aef626db1ae2c7f6e6d0c2e0bcf3684de6b7ab
misung.dev <misung.dev@gmail.com>
2024-08-02 02:38:10 +0900

    #49 [FEAT] 타이틀 및 input창 style 수정

--- src/pages/MyPage/ChangePasswordPage/ChangePasswordPage.jsx
+++ src/pages/MyPage/ChangePasswordPage/ChangePasswordPage.jsx
@@ -1,51 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './ChangePasswordPage.module.css';
-import arrowBackIcon from '../../../assets/icon-arrow-back.svg';
-import eyeIcon from '../../../assets/icon-eye.svg';
+import Icon from '../../../components/Icon/Icon';
 
 export default function ChangePasswordPage() {
   return (
     <main className={styles.changePasswordPage}>
       <div className={styles.topContainer}>
-        <Link to='/my-page' className={styles.goBackBtn}>
-          <img src={arrowBackIcon} alt='뒤로가기' />
-        </Link>
+        <Icon id='arrow-back' />
         <p className={styles.completeBtn}>완료</p>
       </div>
 
       <div className={styles.contentContainer}>
-        <div className={styles.pwContent}>
+        <div className={styles.passwordWrapper}>
           <h3 className={styles.title}>현재 비밀번호</h3>
-          <div className={styles.pwBox}>
-            <input
-              type='password'
-              className={styles.pwInput}
-              placeholder='비밀번호 내용'
-            />
-            <img src={eyeIcon} alt='눈 아이콘' className={styles.eyeIcon} />
+          <div className={styles.password}>
+            <input type='password' className={styles.pwInput} />
+            <Icon id='opened-eye' />
           </div>
         </div>
-        <div className={styles.pwContent}>
+        <div className={styles.passwordWrapper}>
           <h3 className={styles.title}>새 비밀번호</h3>
-          <div className={styles.pwBox}>
-            <input
-              type='password'
-              className={styles.pwInput}
-              placeholder='비밀번호 내용'
-            />
-            <img src={eyeIcon} alt='눈 아이콘' className={styles.eyeIcon} />
+          <div className={styles.password}>
+            <input type='password' className={styles.pwInput} />
+            <Icon id='opened-eye' />
           </div>
         </div>
-        <div className={styles.pwContent}>
+        <div className={styles.passwordWrapper}>
           <h3 className={styles.title}>비밀번호 확인</h3>
-          <div className={styles.pwBox}>
-            <input
-              type='password'
-              className={styles.pwInput}
-              placeholder='비밀번호 내용'
-            />
-            <img src={eyeIcon} alt='눈 아이콘' className={styles.eyeIcon} />
+          <div className={styles.password}>
+            <input type='password' className={styles.pwInput} />
+            <Icon id='opened-eye' />
           </div>
         </div>
       </div>
