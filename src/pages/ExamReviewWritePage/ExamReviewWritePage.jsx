387654b90b9d72b6e2765b0accdae41feef702a5
haesa <xdhaesa@gmail.com>
2024-08-01 16:07:43 +0900

    #64 [FEAT] 시험 후기 작성 페이지 중 첨부파일 등록

--- src/pages/ExamReviewWritePage/ExamReviewWritePage.jsx
+++ src/pages/ExamReviewWritePage/ExamReviewWritePage.jsx
@@ -12,10 +12,13 @@ import {
   TEST_CATEGORY,
   YEARS,
 } from '../../constants';
+import Icon from '../../components/Icon/Icon';
 import InputList from '../../components/Input/InputList/InputList.jsx';
 import InputItem from '../../components/Input/InputItem/InputItem.jsx';
 import Textarea from '../../components/Fieldset/Textarea/Textarea.jsx';
 import styles from './ExamReviewWritePage.module.css';
+
+const FILE_MAX_SIZE = 1000 * 1000 * 10;
 
 export default function ExamReviewWritePage() {
   const [lectureName, setLectureName] = useState('');
@@ -27,6 +30,16 @@ export default function ExamReviewWritePage() {
   const [isPF, setIsPF] = useState(false);
   const [classNumber, setClassNumber] = useState();
   const [description, setDescription] = useState('');
+  const [file, setFile] = useState();
+
+  const handleFile = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile?.size > FILE_MAX_SIZE) {
+      alert('파일은 최대 10MB까지 업로드 할 수 있습니다.');
+      return;
+    }
+    setFile(selectedFile);
+  };
 
   return (
     <main className={styles.main}>
@@ -107,6 +120,16 @@ export default function ExamReviewWritePage() {
           placeholder='강의 시험 유형 및 부가적인 설명을 기술해주세요'
         />
       </CategoryFieldset>
+      <div className={styles.file}>
+        <div className={styles.left}>
+          <Icon id='file' width={18} height={19} />
+          <span className={styles.tag}>첨부파일</span>
+        </div>
+        <div className={styles.right}>
+          <label htmlFor='file'>{file?.name ?? '첨부된 파일이 없어요'}</label>
+          <input id='file' type='file' accept='.pdf' onChange={handleFile} />
+        </div>
+      </div>
     </main>
   );
 }
