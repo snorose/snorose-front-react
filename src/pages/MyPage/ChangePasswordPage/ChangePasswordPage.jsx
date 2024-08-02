b5ff05542ea5d83270260c4e4d7fafa2fc73e532
misung.dev <misung.dev@gmail.com>
2024-08-02 12:21:42 +0900

    #50 [FEAT] 아이콘 클릭시 비밀번호 visible 기능 구현

--- src/pages/MyPage/ChangePasswordPage/ChangePasswordPage.jsx
+++ src/pages/MyPage/ChangePasswordPage/ChangePasswordPage.jsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import styles from './ChangePasswordPage.module.css';
 import Icon from '../../../components/Icon/Icon';
 import BackAppBar from '../../../components/BackAppBar/BackAppBar';
 
 export default function ChangePasswordPage() {
+  const [currentPasswordType, setCurrentPasswordType] = useState({
+    type: 'password',
+    visible: false,
+  });
+  const [newPasswordType, setNewPasswordType] = useState({
+    type: 'password',
+    visible: false,
+  });
+  const [newPasswordCheckType, setNewPasswordCheckType] = useState({
+    type: 'password',
+    visible: false,
+  });
+
+  const handlePasswordType = (setter) => {
+    setter((prev) => ({
+      type: prev.visible ? 'password' : 'text',
+      visible: !prev.visible,
+    }));
+  };
+
   return (
     <main className={styles.changePasswordPage}>
       <BackAppBar />
 
       <div className={styles.contentContainer}>
-        {['현재 비밀번호', '새 비밀번호', '비밀번호 확인'].map((item) => (
-          <div className={styles.passwordWrapper}>
-            <h3 className={styles.title} key={item}>
-              {item}
-            </h3>
-            <div className={styles.password}>
-              <input type='password' className={styles.pwInput} />
-              <Icon id='opened-eye' />
-            </div>
+        <div className={styles.passwordWrapper}>
+          <h3 className={styles.title}>현재 비밀번호</h3>
+          <div className={styles.password}>
+            <input
+              type={currentPasswordType.type}
+              className={styles.passwordInput}
+            />
+            <Icon
+              onClick={() => handlePasswordType(setCurrentPasswordType)}
+              id={currentPasswordType.visible ? 'closed-eye' : 'opened-eye'}
+            />
           </div>
-        ))}
+        </div>
+
+        <div className={styles.passwordWrapper}>
+          <h3 className={styles.title}>새 비밀번호</h3>
+          <div className={styles.password}>
+            <input
+              type={newPasswordType.type}
+              className={styles.passwordInput}
+            />
+            <Icon
+              onClick={() => handlePasswordType(setNewPasswordType)}
+              id={newPasswordType.visible ? 'closed-eye' : 'opened-eye'}
+            />
+          </div>
+        </div>
+
+        <div className={styles.passwordWrapper}>
+          <h3 className={styles.title}>비밀번호 확인</h3>
+          <div className={styles.password}>
+            <input
+              type={newPasswordCheckType.type}
+              className={styles.passwordInput}
+            />
+            <Icon
+              onClick={() => {
+                handlePasswordType(setNewPasswordCheckType);
+              }}
+              id={newPasswordCheckType.visible ? 'closed-eye' : 'opened-eye'}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
