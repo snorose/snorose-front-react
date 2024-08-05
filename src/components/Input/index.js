66a6e32c41f9b9ca19aa50e6e0aeb350dafb42d1
haesa <xdhaesa@gmail.com>
2024-08-06 08:07:46 +0900

    #64 [MERGE] icon.svg 충돌 해결

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,3 +1,3 @@
-export { default as Input } from './Input';
+export { default as Input } from './Input/Input';
 export { default as InputItem } from './InputItem/InputItem';
 export { default as InputList } from './InputList/InputList';
