ef31daf7a749578668f9c5bf1192c1c2f41ac5c8
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-08-07 22:17:49 +0900

    #94 [FEAT] 공지글 리스트 페이지 UI 구현

---
+++ src/pages/NoticeListPage/NoticeListPage.jsx
@@ -0,0 +1,45 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import styles from './NoticeListPage.module.css';
+import { Icon } from '../../components/Icon';
+import { BackAppBar } from '../../components/AppBar';
+import { NOTICE_LIST } from '../../dummy/data/noticeList.js';
+import { NoticeBar } from '../../components/NoticeBar';
+
+export default function NoticeListPage() {
+  const navigate = useNavigate();
+  const handleNavClick = (to) => {
+    return () => {
+      navigate(to);
+    };
+  };
+
+  const boardMap = {
+    'first-snow': '첫눈온방',
+    'large-snow': '함박눈방',
+    'permanent-snow': '만년설방',
+    besookt: '베숙트',
+  };
+
+  const { pathname } = useLocation();
+  const currentPath = pathname.split('/')[2];
+  const currentBoard = boardMap[currentPath] || '';
+
+  return (
+    <div className={styles.container}>
+      <BackAppBar title={currentBoard} hasNotice={true} />
+      <div className={styles.top}>
+        <div
+          className={styles.notification_bar}
+          onClick={handleNavClick('/notice')}
+        >
+          <Icon id='notice-bell' width={11} height={13} />
+          <p>[필독] 공지사항</p>
+        </div>
+      </div>
+      <div className={styles.content}>
+        {NOTICE_LIST &&
+          NOTICE_LIST.map((post) => <NoticeBar key={post.postId} data={post} />)}
+      </div>
+    </div>
+  );
+}
--- src/pages/NoticePage/NoticePage.jsx
+++
@@ -1,3 +0,0 @@
-export default function NoticePage() {
-  return <div>Notice</div>;
-}
