f0f2b5de25ab2990521ee7ee3ec84cb7f4b7b154
misung.dev <misung.dev@gmail.com>
2024-08-08 16:14:00 +0900

    #95 [FEAT] 알림 상태에 따른 아이콘 변경

--- src/pages/AlertPage/AlertPage.jsx
+++ src/pages/AlertPage/AlertPage.jsx
@@ -5,36 +5,35 @@ import { MenuIcon } from '../../components/MenuIcon';
 
 const alertData = [
   {
-    icon: 'star-circle',
-    title: '내 글이 베송트에 추가되었습니다',
+    type: 'addedToBoard',
+    title: '내 글이 베숙트에 추가되었습니다',
     description: `‘스노르즈 포인트를 모으는 5가지 방법’ 글이 베송트에 추가되었습니다.`,
     isRead: false,
     isWarning: false,
   },
   {
-    icon: 'point-circle',
-    title: '베송트 포인트가 적립되었습니다',
+    type: 'pointEarned',
+    title: '베숙트 포인트가 적립되었습니다',
     description: `좋아요 10개를 받아 50포인트가 적립되었습니다.`,
     isRead: false,
     isWarning: false,
   },
-
   {
-    icon: 'comment-circle',
+    type: 'commentReply',
     title: '스노르즈 포인트를 모으는 5가지 방법',
     description: `내가 단 댓글에 답글이 달렸습니다.`,
     isRead: false,
     isWarning: false,
   },
   {
-    icon: 'triangle-exclamation-circle',
+    type: 'reportReward',
     title: '신고 포상 포인트가 적립되었습니다',
     description: `시험 후기 게시글 신고가 완료되어 20포인트가 적립되었습니다.`,
     isRead: true,
     isWarning: false,
   },
   {
-    icon: 'snow-circle-pink',
+    type: 'announcement',
     title: '족보 거래 목격시 즉시 신고해주세요',
     description: `최근 기밀사항을 알아 폭보 거래 제보가 증가하고 있습니다.
 족보 거래를 목격하시면 즉시 신고하고 100포인트를 적립하세요.`,
@@ -42,7 +41,7 @@ const alertData = [
     isWarning: true,
   },
   {
-    icon: 'snow-circle-blue',
+    type: 'announcement',
     title: '족보 거래 목격시 즉시 신고해주세요',
     description: `최근 기밀사항을 알아 폭보 거래 제보가 증가하고 있습니다.
 족보 거래를 목격하시면 즉시 신고하고 100포인트를 적립하세요.`,
@@ -50,7 +49,7 @@ const alertData = [
     isWarning: true,
   },
   {
-    icon: 'snow-circle-blue',
+    type: 'announcement',
     title: '시험후기 오픈 일정 알림',
     description: `시험후기 게시판 오픈일정 안내입니다. (24.12.20 - 25.01.15)
 자세한 내용은 공지사항을 확인하세요.`,
@@ -58,6 +57,23 @@ const alertData = [
     isWarning: false,
   },
 ];
+
+const getIconId = (type, isRead) => {
+  switch (type) {
+    case 'addedToBoard':
+      return 'star-circle';
+    case 'pointEarned':
+      return 'point-circle';
+    case 'commentReply':
+      return 'comment-circle';
+    case 'reportReward':
+      return 'triangle-exclamation-circle';
+    case 'announcement':
+      return isRead ? 'snow-circle-blue' : 'snow-circle-pink';
+    default:
+      return 'default-icon';
+  }
+};
 
 export default function AlertPage() {
   return (
@@ -85,7 +101,7 @@ export default function AlertPage() {
               }`}
             >
               <div className={styles.alertIconContentWrapper}>
-                <Icon id={item.icon} fill='none' />
+                <Icon id={getIconId(item.type, item.isRead)} fill='none' />
                 <div className={styles.alertContent}>
                   <h2
                     className={`${styles.alertTitle} ${
