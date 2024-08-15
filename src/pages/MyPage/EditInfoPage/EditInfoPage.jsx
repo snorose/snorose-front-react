cb34ae1bfc491c6440c84c59901f82f9f1aecb02
misung.dev <misung.dev@gmail.com>
2024-08-15 21:57:31 +0900

    #69 [STYLE] 내 정보 수정 페이지 내 AppBar 컴포넌트로 대체

--- src/pages/MyPage/EditInfoPage/EditInfoPage.jsx
+++ src/pages/MyPage/EditInfoPage/EditInfoPage.jsx
@@ -1,9 +1,8 @@
+import styles from './EditInfoPage.module.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import { Icon } from '../../../components/Icon';
-
-import styles from './EditInfoPage.module.css';
+import { BackAppBar, ActionButton } from '../../../components/AppBar';
 
 export default function EditInfoPage() {
   const [name, setName] = useState('');
@@ -44,99 +43,101 @@ export default function EditInfoPage() {
 
   return (
     <main className={styles.editInfoPage}>
-      <div className={styles.topContainer}>
-        <Link to='/my-page' className={styles.goBackBtn}>
-          <div className={styles.arrowBackIconWrapper}>
-            <Icon id='arrow-back' />
-          </div>
-        </Link>
-        <button className={styles.completionBtn}>완료</button>
-      </div>
+      <header className={styles.topContainer}>
+        <p>
+          <BackAppBar />
+        </p>
+        <div className={styles.submitBtn}>
+          <ActionButton>완료</ActionButton>
+        </div>
+      </header>
 
-      <div className={styles.profileContainer}>
-        <div
-          className={styles.profileImg}
-          onClick={() => document.getElementById('profileImageInput').click()}
-        >
-          {profileImage ? (
-            <img
-              src={profileImage}
-              alt='프로필'
-              className={styles.profilePreview}
-            />
-          ) : (
-            <Icon id='profile-basic-camera' />
-          )}
-        </div>
-        <input
-          type='file'
-          id='profileImageInput'
-          style={{ display: 'none' }}
-          accept='image/*'
-          onChange={handleProfileImageChange}
-        />
-      </div>
-
-      <div className={styles.infoContainer}>
-        <div className={styles.info}>
-          <h3 className={styles.title}>이름</h3>
+      <section className={styles.profileContainer}>
+        <div className={styles.profileImgContainer}>
           <div
-            className={`${styles.inputWrapper} ${nameError ? styles.errorInputWrapper : ''}`}
+            className={styles.profileImg}
+            onClick={() => document.getElementById('profileImageInput').click()}
           >
-            <input
-              type='text'
-              className={`${styles.inputText} ${nameError ? styles.errorInputText : ''}`}
-              placeholder='이름을 입력하세요'
-              value={name}
-              onChange={handleNameChange}
-            />
-            {nameError && <p className={styles.errorMessage}>{nameError}</p>}
-          </div>
-        </div>
-        <div className={styles.info}>
-          <h3 className={styles.title}>생년월일</h3>
-          <div className={styles.inputWrapper}>
-            <input
-              type='text'
-              className={styles.inputText}
-              placeholder='생년월일을 선택하세요'
-            />
-            <div className={styles.calendarIconWrapper}>
-              <Icon id='calendar' />
-            </div>
-          </div>
-        </div>
-        <div className={styles.info}>
-          <h3 className={styles.title}>닉네임</h3>
-          <div
-            className={`${styles.inputWrapper} ${nicknameError ? styles.errorInputWrapper : ''}`}
-          >
-            <input
-              type='text'
-              className={`${styles.inputText} ${nicknameError ? styles.errorInputText : ''}`}
-              placeholder='닉네임을 입력하세요'
-              value={nickname}
-              onChange={handleNicknameChange}
-            />
-            {nicknameError && (
-              <p className={styles.errorMessage}>{nicknameError}</p>
+            {profileImage ? (
+              <img
+                src={profileImage}
+                alt='프로필'
+                className={styles.profilePreview}
+              />
+            ) : (
+              <Icon id='profile-basic-camera' />
             )}
           </div>
+          <input
+            type='file'
+            id='profileImageInput'
+            style={{ display: 'none' }}
+            accept='image/*'
+            onChange={handleProfileImageChange}
+          />
         </div>
-        <div className={styles.info}>
-          <h3 className={styles.title}>전공</h3>
-          <div className={styles.inputWrapper}>
-            <input
-              type='text'
-              className={styles.inputText}
-              placeholder='전공을 선택하세요'
-            />
-            <div className={styles.arrowDownIconWrapper}>
-              <Icon id='angle-down' />
+
+        <div className={styles.infoContainer}>
+          <div className={styles.info}>
+            <h3 className={styles.title}>이름</h3>
+            <div
+              className={`${styles.inputWrapper} ${nameError ? styles.errorInputWrapper : ''}`}
+            >
+              <input
+                type='text'
+                className={`${styles.inputText} ${nameError ? styles.errorInputText : ''}`}
+                placeholder='이름을 입력하세요'
+                value={name}
+                onChange={handleNameChange}
+              />
+              {nameError && <p className={styles.errorMessage}>{nameError}</p>}
+            </div>
+          </div>
+          <div className={styles.info}>
+            <h3 className={styles.title}>생년월일</h3>
+            <div className={styles.inputWrapper}>
+              <input
+                type='text'
+                className={styles.inputText}
+                placeholder='생년월일을 선택하세요'
+              />
+              <div className={styles.calendarIconWrapper}>
+                <Icon id='calendar' />
+              </div>
+            </div>
+          </div>
+          <div className={styles.info}>
+            <h3 className={styles.title}>닉네임</h3>
+            <div
+              className={`${styles.inputWrapper} ${nicknameError ? styles.errorInputWrapper : ''}`}
+            >
+              <input
+                type='text'
+                className={`${styles.inputText} ${nicknameError ? styles.errorInputText : ''}`}
+                placeholder='닉네임을 입력하세요'
+                value={nickname}
+                onChange={handleNicknameChange}
+              />
+              {nicknameError && (
+                <p className={styles.errorMessage}>{nicknameError}</p>
+              )}
+            </div>
+          </div>
+          <div className={styles.info}>
+            <h3 className={styles.title}>전공</h3>
+            <div className={styles.inputWrapper}>
+              <input
+                type='text'
+                className={styles.inputText}
+                placeholder='전공을 선택하세요'
+              />
+              <div className={styles.arrowDownIconWrapper}>
+                <Icon id='angle-down' />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
