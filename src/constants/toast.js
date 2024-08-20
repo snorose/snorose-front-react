b929fff28efc2eef823d9a4ac7d4e4583aa606d9
haesa <xdhaesa@gmail.com>
2024-08-20 15:09:59 +0900

    #140 [FEAT] 시험후기 작성 시 포인트 증가 API 연결

--- src/constants/toast.js
+++ src/constants/toast.js
@@ -1,8 +1,8 @@
 const TOAST = Object.freeze({
-  test: { id: 'test', message: '토스트 테스트 중입니다!' },
-  test2: { id: 'test2', message: '토스트 메시지 여러 개 띄우기 가능??' },
-  emptyTitle: { id: 'empty-title', message: '제목을 입력하세요' },
-  emptyText: { id: 'empty-text', message: '내용을 입력하세요' },
+  EXAM_REVIEW_CREATE: {
+    id: 'EXAM_REVIEW_CREATE',
+    message: '100P 적립이 완료되었어요',
+  },
 });
 
 export { TOAST };
