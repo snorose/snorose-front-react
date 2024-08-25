1c74bb4c69e7b5e877c3aea24224325c656ec1e7
haesa <xdhaesa@gmail.com>
2024-08-25 14:37:53 +0900

    #169 [FEAT] useScrap에 게시글 스크랩일 경우를 위한 조건문 추가

--- src/hooks/useScrap.jsx
+++ src/hooks/useScrap.jsx
@@ -1,23 +1,35 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { useQueryClient, useMutation } from '@tanstack/react-query';
 
 import { scrap as ScrapApi, deleteScrap as deleteScrapApi } from '../apis';
 
 export default function useScrap() {
   const { postId } = useParams();
+  const { pathname } = useLocation();
+
+  const currentBoard = pathname.split('/')[2];
+
   const queryClient = useQueryClient();
 
   const scrap = useMutation({
     mutationFn: () => ScrapApi({ postId }),
     onSuccess: () => {
-      queryClient.invalidateQueries(['reviewDetail', postId]);
+      if (currentBoard === 'exam-review') {
+        queryClient.invalidateQueries(['reviewDetail', postId]);
+      } else {
+        queryClient.invalidateQueries(['postContent', postId]);
+      }
     },
   });
 
   const deleteScrap = useMutation({
     mutationFn: () => deleteScrapApi({ postId }),
     onSuccess: () => {
-      queryClient.invalidateQueries(['reviewDetail', postId]);
+      if (currentBoard === 'exam-review') {
+        queryClient.invalidateQueries(['reviewDetail', postId]);
+      } else {
+        queryClient.invalidateQueries(['postContent', postId]);
+      }
     },
   });
 
