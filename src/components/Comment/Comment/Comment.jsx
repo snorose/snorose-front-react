599e98bfbedb4c202f387530b6e681d62f4194e8
haesa <xdhaesa@gmail.com>
2024-08-29 01:14:01 +0900

    #198 [MODIFY] 삭제된 댓글의 대댓글, 좋아요 아이콘 숨기기

--- src/components/Comment/Comment/Comment.jsx
+++ src/components/Comment/Comment/Comment.jsx
@@ -40,8 +40,6 @@ export default function Comment({ data }) {
   };
 
   const {
-    id,
-    postId,
     userDisplay,
     isWriter,
     isWriterWithdrawn,
@@ -49,7 +47,6 @@ export default function Comment({ data }) {
     isLiked,
     likeCount,
     createdAt,
-    updatedAt,
     isVisible,
     isUpdated,
     isDeleted,
@@ -92,22 +89,26 @@ export default function Comment({ data }) {
           {isDeleted && '(삭제된 댓글입니다)'}
         </div>
         <div className={styles.commentBottom}>
-          <button className={styles.commentCount} onClick={handleReply}>
-            <Icon id='comment' width='15' height='13' fill='#D9D9D9' />
-            <p>{children.length}</p>
-          </button>
-          <button
-            className={styles.likedCount}
-            onClick={() => (isLiked ? deleteLike.mutate() : like.mutate())}
-          >
-            <Icon
-              id='like'
-              width='13'
-              height='12'
-              fill={isLiked ? '#5F86BF' : '#D9D9D9'}
-            />
-            <span>{data.likeCount.toLocaleString()}</span>
-          </button>
+          {!isDeleted && (
+            <>
+              <button className={styles.commentCount} onClick={handleReply}>
+                <Icon id='comment' width='15' height='13' fill='#D9D9D9' />
+                <p>{children.length}</p>
+              </button>
+              <button
+                className={styles.likedCount}
+                onClick={() => (isLiked ? deleteLike.mutate() : like.mutate())}
+              >
+                <Icon
+                  id='like'
+                  width='13'
+                  height='12'
+                  fill={isLiked ? '#5F86BF' : '#D9D9D9'}
+                />
+                <span>{likeCount.toLocaleString()}</span>
+              </button>
+            </>
+          )}
         </div>
         {children.length > 0 &&
           children.map((childComment, index) => (
