9e13590f5f7d220e323d740517a864a7f6dc6cc4
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-09-08 00:54:29 +0900

    #294 [DELETE] 댓글 작성/삭제 포인트 API 제거

--- src/hooks/useComment.jsx
+++ src/hooks/useComment.jsx
@@ -2,17 +2,14 @@ import { useParams } from 'react-router-dom';
 import { useQueryClient, useQuery, useMutation } from '@tanstack/react-query';
 
 import { useToast } from '@/hooks';
-import { TOAST, POINT_CATEGORY_ENUM, POINT_SOURCE_ENUM } from '@/constants';
+import { TOAST } from '@/constants';
 
 import {
   deleteComment as remove,
   getCommentList,
   postComment as post,
   editComment as edit,
-  updatePoint,
 } from '@/apis';
-
-import { USER } from '@/dummy/data';
 
 export default function useComment() {
   const { postId } = useParams();
@@ -28,16 +25,7 @@ export default function useComment() {
   // 댓글 생성
   const postComment = useMutation({
     mutationFn: async ({ content, parentId }) => {
-      const response = await post({ postId, parentId, content });
-      const newCommentId = response.data.result.id;
-      if (response.status === 201) {
-        await updatePoint({
-          userId: USER.userId, // 실제 userId로 교체해야 합니다.
-          category: POINT_CATEGORY_ENUM.COMMENT_CREATE,
-          source: POINT_SOURCE_ENUM.COMMENT,
-          sourceId: newCommentId,
-        });
-      }
+      await post({ postId, parentId, content });
     },
     onSuccess: () => {
       toast(TOAST.COMMENT.create);
@@ -51,15 +39,7 @@ export default function useComment() {
   // 댓글 삭제
   const deleteComment = useMutation({
     mutationFn: async ({ commentId }) => {
-      const response = await remove({ postId, commentId });
-      if (response.status === 200) {
-        await updatePoint({
-          userId: USER.userId, // 실제 userId로 교체해야 합니다.
-          category: POINT_CATEGORY_ENUM.COMMENT_DELETE,
-          source: POINT_SOURCE_ENUM.COMMENT,
-          sourceId: commentId,
-        });
-      }
+      await remove({ postId, commentId });
     },
     onSuccess: () => {
       toast(TOAST.COMMENT.delete);
