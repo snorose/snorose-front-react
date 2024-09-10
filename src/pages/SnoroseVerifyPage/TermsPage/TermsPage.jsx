99b16e5291141c05c03cb321196eeb512aa2dca0
haesa <xdhaesa@gmail.com>
2024-09-11 03:31:57 +0900

    #314 [FEAT] 스노로즈 인증 신청 확정 이용약관으로 교체

--- src/pages/SnoroseVerifyPage/TermsPage/TermsPage.jsx
+++ src/pages/SnoroseVerifyPage/TermsPage/TermsPage.jsx
@@ -5,10 +5,20 @@ import { TERMS } from '../../../constants';
 import styles from './TermsPage.module.css';
 
 export default function TermsPage({ setStep }) {
+  const { text, list } = TERMS;
+  const [startTerms, endTerms] = text;
+
   return (
     <section className={styles.content}>
       <div className={styles.terms}>
-        <pre className={styles.text}>{TERMS}</pre>
+        <pre className={styles.text}>
+          {startTerms}
+          {list.map((item) => (
+            <li className={styles.item}>{item}</li>
+          ))}
+          <br />
+          {endTerms}
+        </pre>
       </div>
       <Button onClick={() => setStep('verify')}>동의</Button>
     </section>
