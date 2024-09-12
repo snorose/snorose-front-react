13e37776868052e264a5527204724945553443b7
haesa <xdhaesa@gmail.com>
2024-09-13 04:56:22 +0900

    #342 [FIX] 시험후기 필터 유지되도록 수정

--- src/pages/ExamReviewPage/ExamReviewPage/ExamReviewPage.jsx
+++ src/pages/ExamReviewPage/ExamReviewPage/ExamReviewPage.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { getReviewList } from '@/apis';
 
@@ -9,22 +9,39 @@ import { ExamReviewList, ExamReviewSearchList } from '@/pages/ExamReviewPage';
 
 import { AppBar, DropDownBlue, PTR, Search, WriteButton } from '@/components';
 
+import { convertToObject } from '@/utils';
 import { YEARS, SEMESTERS, EXAM_TYPES } from '@/constants';
 
 import styles from './ExamReviewPage.module.css';
 
+const LECTURE_YEAR_LEBEL = convertToObject(YEARS);
+const SEMESTER_LEBEL = convertToObject(SEMESTERS);
+const EXAM_TYPE_LEBEL = convertToObject(EXAM_TYPES);
+
 export default function ExamReviewPage() {
-  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const { pathname, search } = useLocation();
   const urlKeyword = decodeURIComponent(pathname.split('/')[4] || '');
+  const queryParams = new URLSearchParams(search);
 
   const [isOpen, setIsOpen] = useState({
     year: false,
     semester: false,
     examType: false,
   });
-  const [lectureYear, setLectureYear] = useState();
-  const [semester, setSemester] = useState();
-  const [examType, setExamType] = useState();
+
+  const [lectureYear, setLectureYear] = useState({
+    id: queryParams.get('lectureYear'),
+    name: LECTURE_YEAR_LEBEL[queryParams.get('lectureYear')],
+  });
+  const [semester, setSemester] = useState({
+    id: queryParams.get('semester'),
+    name: SEMESTER_LEBEL[queryParams.get('semester')],
+  });
+  const [examType, setExamType] = useState({
+    id: queryParams.get('examType'),
+    name: EXAM_TYPE_LEBEL[queryParams.get('examType')],
+  });
 
   const reviewResult = usePagination({
     queryKey: ['reviewList'],
@@ -42,6 +59,27 @@ export default function ExamReviewPage() {
   });
 
   const { handleChange, handleOnKeyDown, keyword } = searchResult;
+
+  useEffect(() => {
+    const filterOption = {
+      lectureYear: lectureYear?.id,
+      semester: semester?.id,
+      examType: examType?.id,
+    };
+    const param = Object.entries(filterOption).reduce(
+      (result, [key, value]) => (value ? `${result}&${key}=${value}` : result),
+      ''
+    );
+
+    if (keyword === '') {
+      navigate(`/board/exam-review`);
+      return;
+    }
+
+    navigate(
+      `/board/exam-review/search/${encodeURIComponent(keyword)}?${param}`
+    );
+  }, [lectureYear, semester, examType]);
 
   return (
     <main>
