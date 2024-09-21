27141369d5fa039aee2dda77e3c84dc8f37554d1
haesa <xdhaesa@gmail.com>
2024-09-22 02:01:56 +0900

    #477 [REFACTOR] CSS px 단위를 rem으로 변경

--- src/components/CategoryBoard/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CategoryBoard } from './CategoryBoard/CategoryBoard';
-export { default as CategoryBoardItem } from './CategoryBoardItem/CategoryBoardItem';
-export { default as CategoryBoardItemDetail } from './CategoryBoardItemDetail/CategoryBoardItemDetail';
--- src/components/Sponsor/Sponsor.jsx
+++
@@ -1,7 +0,0 @@
-import sponsor from '@/assets/images/sponsor.svg';
-
-export default function Sponsor(props) {
-  return (
-    <img src={sponsor} alt='sponsor' style={{ height: '74px' }} {...props} />
-  );
-}
