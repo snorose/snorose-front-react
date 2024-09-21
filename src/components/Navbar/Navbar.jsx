67f78aac90b4b3c5fea85328d50074d1a6c644a2
haesa <xdhaesa@gmail.com>
2024-09-21 21:30:40 +0900

    #470 [FIX] 시험후기 검색에서 Navbar의 강조가 풀리는 버그 수정

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,17 +6,13 @@ import { NAVBAR_MENUS } from '@/constants';
 
 import styles from './Navbar.module.css';
 
-const NAVBAR_ID = {
-  '/': 'home',
-  '/home': 'home',
-  '/board': 'board',
-  '/board/exam-review': 'test',
-  // '/alert': 'bell',
-  '/my-page': 'mypage',
-};
-
 export default function Navbar() {
   const { pathname } = useLocation();
+
+  const isActive = ({ id, to }) =>
+    pathname === to ||
+    (id === 'home' && pathname === '') ||
+    (id === 'test' && pathname.startsWith('/board/exam-review/search'));
 
   return (
     <nav className={styles.nav}>
@@ -26,16 +22,12 @@ export default function Navbar() {
             <Link key={id} to={to}>
               <li className={styles.menu}>
                 <Icon
-                  id={id === NAVBAR_ID[pathname] ? `${id}-fill` : id}
+                  id={isActive({ id, to }) ? `${id}-fill` : id}
                   {...props}
                 />
                 <span
                   style={{
-                    color:
-                      id === NAVBAR_ID[pathname] ||
-                      (pathname === '' && id === 'home')
-                        ? '#00368E'
-                        : '#5F86BF',
+                    color: isActive({ id, to }) ? '#00368E' : '#5F86BF',
                   }}
                 >
                   {label}
