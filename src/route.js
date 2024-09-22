d9f163b17c2c934644aa0386ca923fc506bc6179
haesa <xdhaesa@gmail.com>
2024-09-22 19:00:03 +0900

    #480 [MODIFY] 블랙리스트 회원 등급 추가 및 마이페이지에 누락된 ProtectedRoute 추가

--- src/route.js
+++ src/route.js
@@ -59,7 +59,13 @@ const getRolesForReadBoard = (boardPath) => {
     case 'all':
       return [ROLE.preUser, ROLE.user, ROLE.admin, ROLE.official];
     case 'notice':
-      return [ROLE.preUser, ROLE.user, ROLE.admin, ROLE.official];
+      return [
+        ROLE.preUser,
+        ROLE.user,
+        ROLE.admin,
+        ROLE.official,
+        ROLE.blacklist,
+      ];
     default:
       return [];
   }
@@ -209,13 +215,6 @@ export const routeList = [
       },
       ...boardRoutes,
       {
-        path: '/board/all/search',
-        element: <PostSearchPage />,
-        meta: {
-          hideNav: true,
-        },
-      },
-      {
         path: '/board/all/search/:keyword',
         element: <PostSearchPage />,
         meta: {
@@ -276,10 +275,12 @@ export const routeList = [
       {
         path: '/board/exam-review/:postId/edit',
         element: (
-          <ExamReviewEditPage
+          <ProtectedRoute
             roles={[ROLE.user, ROLE.admin]}
             message={'시험후기 수정 권한이 없습니다.'}
-          />
+          >
+            <ExamReviewEditPage />
+          </ProtectedRoute>
         ),
         meta: {
           hideNav: true,
@@ -288,10 +289,12 @@ export const routeList = [
       {
         path: '/board/exam-review-write',
         element: (
-          <ExamReviewWritePage
+          <ProtectedRoute
             roles={[ROLE.user, ROLE.admin]}
             message={'시험후기 작성 권한이 없습니다.'}
-          />
+          >
+            <ExamReviewWritePage />
+          </ProtectedRoute>
         ),
         meta: {
           hideNav: true,
@@ -321,14 +324,22 @@ export const routeList = [
       },
       {
         path: '/my-page/password',
-        element: <ChangePasswordPage />,
+        element: (
+          <ProtectedRoute>
+            <ChangePasswordPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
       },
       {
         path: '/my-page/edit-info',
-        element: <EditInfoPage />,
+        element: (
+          <ProtectedRoute>
+            <EditInfoPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
@@ -346,7 +357,11 @@ export const routeList = [
       },
       {
         path: '/my-page/delete-account',
-        element: <DeleteAccountPage />,
+        element: (
+          <ProtectedRoute>
+            <DeleteAccountPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
@@ -367,35 +382,55 @@ export const routeList = [
       },
       {
         path: '/my-page/my-post',
-        element: <MyPostPage />,
+        element: (
+          <ProtectedRoute>
+            <MyPostPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
       },
       {
         path: '/my-page/comment',
-        element: <CommentPage />,
+        element: (
+          <ProtectedRoute>
+            <CommentPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
       },
       {
         path: '/my-page/download-exam-review',
-        element: <DownloadExamReviewPage />,
+        element: (
+          <ProtectedRoute>
+            <DownloadExamReviewPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
       },
       {
         path: '/my-page/scrap',
-        element: <ScrapPage />,
+        element: (
+          <ProtectedRoute>
+            <ScrapPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
       },
       {
         path: '/my-page/scrap-exam-review',
-        element: <ScrapExamReviewPage />,
+        element: (
+          <ProtectedRoute>
+            <ScrapExamReviewPage />
+          </ProtectedRoute>
+        ),
         meta: {
           hideNav: true,
         },
