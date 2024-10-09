5129ef42d4cc275b6a3c7993250325529ac60cce
haesa <xdhaesa@gmail.com>
2024-10-09 19:19:20 +0900

    #590 [MODIFY] 숙명인 인증 요청 로딩 시 FetchLoading 노출

--- src/pages/SnoroseVerifyPage/VerifyPage/VerifyPage.jsx
+++ src/pages/SnoroseVerifyPage/VerifyPage/VerifyPage.jsx
@@ -6,7 +6,7 @@ import { useToast } from '@/hooks/index.js';
 
 import { Button, Input } from '@/pages/SnoroseVerifyPage';
 
-import { InputPassword } from '@/components';
+import { FetchLoading, InputPassword } from '@/components';
 
 import { isEmailValid } from '@/utils';
 
@@ -19,6 +19,7 @@ export default function VerifyPage({ setStep }) {
   const [studentId, setStudentId] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState();
 
   const isAllFieldsCompleted = studentId && password && email;
 
@@ -33,6 +34,8 @@ export default function VerifyPage({ setStep }) {
       return;
     }
 
+    setLoading(true);
+
     try {
       await verifySookmyungPortal({
         studentId,
@@ -44,6 +47,8 @@ export default function VerifyPage({ setStep }) {
     } catch ({ response }) {
       const { data } = response;
       toast(Object.values(data)[0]);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -75,6 +80,13 @@ export default function VerifyPage({ setStep }) {
         />
       </div>
       <Button onClick={verify}>인증</Button>
+      {loading && (
+        <div className={styles.loading}>
+          <FetchLoading>
+            <span className={styles.text}>잠시만 기다려주세요</span>
+          </FetchLoading>
+        </div>
+      )}
     </section>
   );
 }
