227e10680b55cd1391197d6fe94de5a4f81e9a76
haesa <xdhaesa@gmail.com>
2024-10-10 05:04:32 +0900

    #608 [FIX] 사파리에서 BoardBar의 배경 이미지가 보이지 않는 문제 수정

--- src/components/BoardBar/BoardBar.jsx
+++ src/components/BoardBar/BoardBar.jsx
@@ -4,14 +4,17 @@ import styles from './BoardBar.module.css';
 
 export default function BoardBar({ data }) {
   return (
-    <Link to={`/board/${data.textId}`} className={styles.board_list_bar}>
-      <svg
-        className={styles.board_bar_background}
-        style={{ backgroundImage: `url("${data.image}")` }}
-      ></svg>
-      <div className={styles.board_bar_text}>
-        <div className={styles.board_bar_title}>{data.title}</div>
-        <div className={styles.board_bar_description}>{data.desc}</div>
+    <Link to={`/board/${data.textId}`}>
+      <div
+        className={styles.container}
+        style={{
+          backgroundImage: `url(${data.image})`,
+        }}
+      >
+        <div className={styles.textBox}>
+          <div className={styles.title}>{data.title}</div>
+          <div className={styles.description}>{data.desc}</div>
+        </div>
       </div>
     </Link>
   );
