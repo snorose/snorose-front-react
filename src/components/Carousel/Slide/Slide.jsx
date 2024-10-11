981f90a324e0b4d2dfda4a6cd8afbf2df5a5994a
haesa <xdhaesa@gmail.com>
2024-10-11 22:24:44 +0900

    #639 [CHORE] Slide에서 Link를 a 태그로 변경
    배너 랜딩 URL이 외부 링크인 경우가 있어 수정했습니다.

--- src/components/Carousel/Slide/Slide.jsx
+++ src/components/Carousel/Slide/Slide.jsx
@@ -3,14 +3,13 @@ import { useState } from 'react';
 import defaultBanner from '@/assets/images/bannerError.svg';
 
 import styles from './Slide.module.css';
-import { Link } from 'react-router-dom';
 
 export default function Slide({ src, redirectUrl, alt }) {
   const [imgSrc, setImgSrc] = useState(src);
 
   return (
-    <Link className={styles.slide} to={redirectUrl}>
+    <a className={styles.slide} href={redirectUrl}>
       <img src={imgSrc} alt={alt} onError={() => setImgSrc(defaultBanner)} />
-    </Link>
+    </a>
   );
 }
