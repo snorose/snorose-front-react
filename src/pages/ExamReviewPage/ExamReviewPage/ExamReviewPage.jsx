b59824bff3fc96ea81fe038d51307420a6b920c0
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-10-12 18:29:51 +0900

    #620 [FIX] 스크롤 위치 저장 hook 시험후기 리스트/검색에 적용

--- src/pages/ExamReviewPage/ExamReviewPage/ExamReviewPage.jsx
+++ src/pages/ExamReviewPage/ExamReviewPage/ExamReviewPage.jsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getReviewList, getNoticeLine } from '@/apis';
 
-import { usePagination, useSearch } from '@/hooks';
+import { usePagination, useSearch, useScrollRestoration } from '@/hooks';
 
 import { ExamReviewList, ExamReviewSearchList } from '@/pages/ExamReviewPage';
 
@@ -20,6 +20,7 @@ const SEMESTER_LEBEL = convertToObject(SEMESTERS);
 const EXAM_TYPE_LEBEL = convertToObject(EXAM_TYPES);
 
 export default function ExamReviewPage() {
+  const { scrollRef, saveScrollPosition } = useScrollRestoration();
   const navigate = useNavigate();
   const { search } = useLocation();
   const queryParams = new URLSearchParams(search);
@@ -90,59 +91,66 @@ export default function ExamReviewPage() {
 
   return (
     <main>
-      <AppBar title='시험후기' />
-      <div className={styles.notification}>
-        <Link
-          className={styles.notification_bar}
-          to={`/board/exam-review/notice`}
-        >
-          <Icon id='notice-bell' width={11} height={13} />
-          <p>[필독]&nbsp;&nbsp;{noticeLineData?.title}</p>
-        </Link>
-      </div>
+      <div className={styles.scrollContainer} ref={scrollRef}>
+        <AppBar title='시험후기' />
+        <div className={styles.notification}>
+          <Link
+            className={styles.notification_bar}
+            to={`/board/exam-review/notice`}
+          >
+            <Icon id='notice-bell' width={11} height={13} />
+            <p>[필독]&nbsp;&nbsp;{noticeLineData?.title}</p>
+          </Link>
+        </div>
 
-      <Search
-        className={styles.search}
-        placeholder='시험후기 검색'
-        keyword={keyword}
-        handleKeyDown={handleOnKeyDown}
-        onChange={handleChange}
-      />
-      <div className={styles.filters}>
-        <DropDownBlue
-          options={YEARS}
-          placeholder='연도'
-          name='year'
-          select={lectureYear}
-          setFn={setLectureYear}
-          isOpen={isOpen['year']}
-          setIsOpen={setIsOpen}
+        <Search
+          className={styles.search}
+          placeholder='시험후기 검색'
+          keyword={keyword}
+          handleKeyDown={handleOnKeyDown}
+          onChange={handleChange}
         />
-        <DropDownBlue
-          options={SEMESTERS}
-          placeholder='학기'
-          name='semester'
-          select={semester}
-          setFn={setSemester}
-          isOpen={isOpen['semester']}
-          setIsOpen={setIsOpen}
-        />
-        <DropDownBlue
-          options={EXAM_TYPES}
-          placeholder='시험 종류'
-          name='examType'
-          select={examType}
-          setFn={setExamType}
-          isOpen={isOpen['examType']}
-          setIsOpen={setIsOpen}
-        />
+        <div className={styles.filters}>
+          <DropDownBlue
+            options={YEARS}
+            placeholder='연도'
+            name='year'
+            select={lectureYear}
+            setFn={setLectureYear}
+            isOpen={isOpen['year']}
+            setIsOpen={setIsOpen}
+          />
+          <DropDownBlue
+            options={SEMESTERS}
+            placeholder='학기'
+            name='semester'
+            select={semester}
+            setFn={setSemester}
+            isOpen={isOpen['semester']}
+            setIsOpen={setIsOpen}
+          />
+          <DropDownBlue
+            options={EXAM_TYPES}
+            placeholder='시험 종류'
+            name='examType'
+            select={examType}
+            setFn={setExamType}
+            isOpen={isOpen['examType']}
+            setIsOpen={setIsOpen}
+          />
+        </div>
+        {urlKeyword === null ? (
+          <ExamReviewList
+            result={reviewResult}
+            saveScrollPosition={saveScrollPosition}
+          />
+        ) : (
+          <ExamReviewSearchList
+            result={searchResult}
+            saveScrollPosition={saveScrollPosition}
+          />
+        )}
       </div>
-
-      {urlKeyword === null ? (
-        <ExamReviewList result={reviewResult} />
-      ) : (
-        <ExamReviewSearchList result={searchResult} />
-      )}
 
       <WriteButton to='/board/exam-review-write' />
     </main>
