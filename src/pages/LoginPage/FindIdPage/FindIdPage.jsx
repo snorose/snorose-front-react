92c8f90797fa077b9bd0218b885481ea4f308ff3
misung.dev <misung.dev@gmail.com>
2024-10-13 21:52:53 +0900

    #683 [FEAT] 아이디 찾기 내 설명글 추가

--- src/pages/LoginPage/FindIdPage/FindIdPage.jsx
+++ src/pages/LoginPage/FindIdPage/FindIdPage.jsx
@@ -84,7 +84,50 @@ export default function FindIdPage() {
                 />
               </div>
             ))}
+
+            <div className={styles.alert}>
+              <ol>
+                <li>
+                  1) 아이디 찾기를 해도 입력한 정보와 일치하는 정보가 없다고
+                  뜹니다.
+                </li>
+                <li>
+                  2) 마이페이지에서 학번이 ‘학번+숫자’형식입니다. 변경하고
+                  싶어요.
+                  <br />
+                </li>
+                <strong>
+                  → 구 스노로즈에 저장된 학번이 아래와 같은 경우, 데이터
+                  정합성을 위하여 임의로 학번+숫자 형식으로 모두 수정되었습니다.
+                </strong>
+                <br />
+                <br />
+                <li>
+                  1) 가입 시 입력한 학번이 정확하지 않은 경우(ex 111111111111,
+                  00000000000 등)
+                </li>
+                <li>
+                  2) 다른 유저가 같은 학번으로 입학한 경우
+                  <br />
+                  <strong>
+                    → 하나의 학번 당 한 유저만 가입 가능하므로 리자가 임의로
+                    다른 학번으로 바꿨습니다.
+                  </strong>
+                </li>
+                <br />
+                <li>
+                  따라서 위의 경우에 해당하시는 분과 학번이 잘못되어 수정을
+                  원하시는 분들은, <strong>아래 구글 폼</strong>을 통해
+                  <br />
+                  <strong>학번 변경 문의</strong>를 남겨주시면
+                  해결해드리겠습니다.
+                  <br />
+                  <a href='https://forms.gle/xwi7q47Dz59UNeEW7'>구글폼 링크</a>
+                </li>
+              </ol>
+            </div>
           </div>
+
           <div className={styles.buttonFrame}>
             <Submit btnName='다음으로' className={submitState()} />
           </div>
--- src/pages/LoginPage/FindPwPage/FindPwPage.jsx
+++ src/pages/LoginPage/FindPwPage/FindPwPage.jsx
@@ -70,7 +70,6 @@ export default function FindPwPage() {
           <div>
             <BackAppBar />
             <h1 className={styles.pageTitle}>비밀번호 찾기</h1>
-
             {inputProps.map((props, i) => (
               <div className={styles.inputFrame} key={i}>
                 <Input
@@ -85,8 +84,9 @@ export default function FindPwPage() {
                 />
               </div>
             ))}
+
             <div className={styles.alert}>
-              <ul style={{ listStyleType: 'circle' }}>
+              <ul>
                 <li>
                   기존 이메일을 알 수 없는 경우, "아이디 찾기" 기능을 통해
                   확인할 수 있습니다.
@@ -98,23 +98,24 @@ export default function FindPwPage() {
                   통해 이메일 주소를 확인할 수 있습니다.
                 </li>
                 <li>
-                  만약 이메일이 존재하지 않거나 유효하지 않을 경우, 아래의 폼을
-                  작성해주시기 바랍니다.
+                  만약 이메일이 존재하지 않거나 유효하지 않을 경우,{' '}
+                  <strong>아래 구글 폼</strong>을 작성해주시기 바랍니다.
                   <br />
                   <a href='https://forms.gle/PDmKuPUuUzKXTh8BA'>구글폼 링크</a>
                 </li>
               </ul>
             </div>
           </div>
-          <div className={styles.buttonFrame}>
-            <Submit btnName='완료' className={submitState()} />
-            <Link to='/find-id'>
-              <div className={styles.findIDButton}>
-                <p>아이디 찾기</p>
-                <Icon id='angle-right' width={24} height={24} />
-              </div>
-            </Link>
-          </div>
+        </div>
+
+        <div className={styles.buttonFrame}>
+          <Submit btnName='완료' className={submitState()} />
+          <Link to='/find-id'>
+            <div className={styles.findIDButton}>
+              <p>아이디 찾기</p>
+              <Icon id='angle-right' width={24} height={24} />
+            </div>
+          </Link>
         </div>
       </form>
       {loading && <FetchLoadingOverlay text={LOADING_MESSAGE.default} />}
