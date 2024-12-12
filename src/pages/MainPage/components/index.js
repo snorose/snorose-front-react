62e9fa280e3e2f3e2fc4a70662a5d365d2888c3b
haesa <xdhaesa@gmail.com>
2024-12-13 02:51:04 +0900

    #760 [FIX] MainPage/components/index에 누락된 HomeCardErrorFallback 경로 추가

--- src/pages/MainPage/components/index.js
+++ src/pages/MainPage/components/index.js
@@ -1,4 +1,5 @@
 export { default as HomeCard } from './HomeCard/HomeCard';
+export { default as HomeCardErrorFallback } from './HomeCard/HomeCardErrorFallback';
 export { default as HomeCardSkeleton } from './HomeCard/HomeCardSkeleton';
 export { default as HomeCommunity } from './HomeCommunity/HomeCommunity';
 export { default as HomeBesookt } from './HomeBesookt/HomeBesookt';
