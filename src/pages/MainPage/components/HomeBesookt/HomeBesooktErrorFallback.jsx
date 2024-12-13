c4cb440a0232a9085c642e303fa9978452414ab2
haesa <xdhaesa@gmail.com>
2024-12-13 19:52:52 +0900

    #760 [FIX] 핸들링 되지 않는 모든 에러 코드에 대해 ServerErrorFallback 컴포넌트 노출

--- src/pages/MainPage/components/HomeBesookt/HomeBesooktErrorFallback.jsx
+++ src/pages/MainPage/components/HomeBesookt/HomeBesooktErrorFallback.jsx
@@ -7,25 +7,22 @@ export default function HomeBesooktErrorFallback({
   resetErrorBoundary,
 }) {
   const { status } = error;
-  let text = '';
-
-  if (status === 500) {
-    return <ServerErrorFallback reset={resetErrorBoundary} />;
-  }
 
   if (status === 401) {
-    text = '로그인 후 이용 가능합니다.';
+    return <Fallback text={'로그인 후 이용 가능합니다.'} />;
   }
 
   if (status === 403) {
-    text = '등업 완료 후 이용 가능합니다.';
+    return <Fallback text={'등업 완료 후 이용 가능합니다.'} />;
   }
 
-  return (
-    <ul className={styles.list}>
-      <li className={styles.item}>{text}</li>
-      <li className={styles.item}>{text}</li>
-      <li className={styles.item}>{text}</li>
-    </ul>
-  );
+  return <ServerErrorFallback reset={resetErrorBoundary} />;
 }
+
+const Fallback = ({ text }) => (
+  <ul className={styles.list}>
+    <li className={styles.item}>{text}</li>
+    <li className={styles.item}>{text}</li>
+    <li className={styles.item}>{text}</li>
+  </ul>
+);
--- src/pages/MainPage/components/HomeCard/HomeCardErrorFallback.jsx
+++ src/pages/MainPage/components/HomeCard/HomeCardErrorFallback.jsx
@@ -1,9 +1,5 @@
 import { ServerErrorFallback } from '@/components';
 
 export default function HomeCardErrorFallback({ error, resetErrorBoundary }) {
-  const { status } = error;
-
-  if (status === 500) {
-    return <ServerErrorFallback reset={resetErrorBoundary} />;
-  }
+  return <ServerErrorFallback reset={resetErrorBoundary} />;
 }
