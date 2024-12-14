da3cf4e371d815c4c6a7638ad4e9ee11d4f85a96
haesa <xdhaesa@gmail.com>
2024-12-14 16:33:40 +0900

    #768 [CHORE] 팝업 공지에서 리뉴얼 만족도 조사 안내 삭제

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,38 +1,30 @@
 import { useState } from 'react';
 
+import { Heading, Content } from '@/components/PopUp/Component.jsx';
+
 import styles from './PopUp.module.css';
 
-const CONTENT = (
-  <>
-    <p className={styles.hello}>
-      ❄️ 안녕하세요. 숙명인을 위한 커뮤니티, 스노로즈입니다. ❄️
-    </p>
+const content = (
+  <div className={styles.notice}>
+    <div>
+      <p className={styles.hello}>
+        ❄️ 안녕하세요. 숙명인을 위한 커뮤니티, 스노로즈입니다. ❄️
+      </p>
+      <Heading>1. 24-2 기말고사 포인트 미지급 기간</Heading>
+      <Content>12.02(월) 00:00 ~ 12.20(금) 23:59</Content>
 
-    <h2 className={styles.title}>1. 24-2 기말고사 포인트 미지급 기간</h2>
-    <p className={styles.content}>12.02(월) 00:00 ~ 12.20(금) 23:59</p>
-
-    <h2 className={styles.title}>2. 시험후기 게시판 운영 기간</h2>
-    <p className={styles.content}>12.21(토) 00:00 ~ 2025.01.20(월) 23:59</p>
-
-    <h2 className={styles.title}>3. 리뉴얼 사이트 만족도 조사 안내</h2>
-    <p className={styles.content}>
-      {`리뉴얼 오픈 1개월을 맞이하여 스노로즈 이용자를 대상으로 종합적인 만족도 조사를 진행하고자 합니다. 스노로즈의 발전을 위한 숙명인 여러분의 소중한 의견을 기다리고 있겠습니다.\n\n`}
-      <li>
-        설문지 링크:{' '}
-        <a className={styles.link} href='https://forms.gle/x6j8SRujTdKbXhk88'>
-          https://forms.gle/x6j8SRujTdKbXhk88
-        </a>
-      </li>
-      <li>설문 기간: 2024.11.12(화) ~ 2024.12.12(목)</li>
-    </p>
-
-    <hr />
-    <p className={styles.content}>
-      ※공식 문의 창구 (이메일(snorose1906@gmail.com), 카카오톡 1:1 문의) 이외의
-      문의는 받고 있지 않습니다. 공식 문의 창구 이외의 문의 글은 답변 없이
-      삭제될 수 있음을 알려드립니다.
-    </p>
-  </>
+      <Heading>2. 시험후기 게시판 운영 기간</Heading>
+      <Content>12.21(토) 00:00 ~ 2025.01.20(월) 23:59</Content>
+    </div>
+    <div>
+      <hr />
+      <Content>
+        ※공식 문의 창구 (이메일(snorose1906@gmail.com), 카카오톡 1:1 문의)
+        이외의 문의는 받고 있지 않습니다. 공식 문의 창구 이외의 문의 글은 답변
+        없이 삭제될 수 있음을 알려드립니다.
+      </Content>
+    </div>
+  </div>
 );
 
 export default function PopUp({ close }) {
@@ -44,7 +36,7 @@ export default function PopUp({ close }) {
     <section className={styles.container}>
       <div className={styles.popUp}>
         <div className={styles.top}>
-          <pre>{CONTENT}</pre>
+          <pre>{content}</pre>
         </div>
         <div className={styles.bottom}>
           <label className={styles.checkBox}>
