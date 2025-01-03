55a6e404d71a645b3fbb19df2680788782d99423
haesa <xdhaesa@gmail.com>
2025-01-03 17:34:45 +0900

    #790 [FEAT] 댓글 하이퍼링크 활성화

--- src/components/Comment/Comment/Comment.jsx
+++ src/components/Comment/Comment/Comment.jsx
@@ -8,7 +8,7 @@ import { DeleteModal, OptionModal, ConfirmModal } from '@/components/Modal';
 import { Icon } from '@/components/Icon';
 import { NestedComment } from '@/components/Comment';
 
-import { timeAgo } from '@/utils';
+import { convertHyperlink, timeAgo } from '@/utils';
 
 import { LIKE_TYPE, MUTATION_KEY } from '@/constants';
 
@@ -132,7 +132,14 @@ const Comment = forwardRef((props, ref) => {
           className={`${styles.commentCenter} ${(isDeleted || !isVisible) && styles.hide}`}
         >
           {!isDeleted &&
-            (isVisible ? content : '(관리자에 의해 차단된 댓글입니다)')}
+            (isVisible ? (
+              <p
+                className={styles.content}
+                dangerouslySetInnerHTML={convertHyperlink(content)}
+              ></p>
+            ) : (
+              '(관리자에 의해 차단된 댓글입니다)'
+            ))}
           {isDeleted && '(삭제된 댓글입니다)'}
         </div>
         <div className={styles.commentBottom}>
