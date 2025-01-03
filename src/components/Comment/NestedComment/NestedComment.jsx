049a2fd1aa8507105d5d1c04a60545054594c59e
haesa <xdhaesa@gmail.com>
2025-01-03 17:34:45 +0900

    #790 [FEAT] 댓글 하이퍼링크 활성화

--- src/components/Comment/NestedComment/NestedComment.jsx
+++ src/components/Comment/NestedComment/NestedComment.jsx
@@ -4,7 +4,7 @@ import { useLike } from '@/hooks';
 
 import { Icon } from '@/components/Icon';
 
-import { timeAgo } from '@/utils';
+import { convertHyperlink, timeAgo } from '@/utils';
 
 import { LIKE_TYPE } from '@/constants';
 
@@ -69,7 +69,14 @@ export default function NestedComment({
           className={`${styles.nestedPadding} ${(isDeleted || !isVisible) && styles.hide}`}
         >
           {!isDeleted &&
-            (isVisible ? content : '(관리자에 의해 차단된 댓글입니다)')}
+            (isVisible ? (
+              <p
+                className={styles.content}
+                dangerouslySetInnerHTML={convertHyperlink(content)}
+              ></p>
+            ) : (
+              '(관리자에 의해 차단된 댓글입니다)'
+            ))}
           {isDeleted && '(삭제된 댓글입니다)'}
         </div>
       </div>
