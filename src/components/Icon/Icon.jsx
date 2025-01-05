e570f1957492f8da67429b261c6db73edf78e07b
haesa <xdhaesa@gmail.com>
2025-01-06 06:10:07 +0900

    #794 [CORRECT] Icon 컴포넌트의 id prop을 required로 지정

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
+
 import iconSvg from '@/assets/icon.svg';
 
-export default function Icon({ id, fill, ...props }) {
+export default function Icon({ id, fill = 'none', ...props }) {
   return (
     <svg {...props}>
       <use href={`${iconSvg}#${id}`} fill={fill} />
     </svg>
   );
 }
+
+Icon.propTypes = {
+  id: PropTypes.string.isRequired,
+};
