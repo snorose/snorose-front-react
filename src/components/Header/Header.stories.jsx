8d62774133887750d1a3a1896b5026ae6c6a3fc9
dawncoding <sunnuri99@gmail.com>
2025-02-19 16:57:10 +0900

    #803 [DOCS] Header 컴포넌트 description 및 decorator 설정 추가

--- src/components/Header/Header.stories.jsx
+++ src/components/Header/Header.stories.jsx
@@ -1,8 +1,24 @@
 import Header from './Header';
 
-export default {
-  title: 'components/Header',
+const headerStoryConfig = {
   component: Header,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Header 컴포넌트는 애플리케이션의 상단 내비게이션 바 역할을 합니다.',
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '600px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
-export const BeforeLogin = () => <Header />;
+export default headerStoryConfig;
+
+export const LoggedOut = () => <Header />;
