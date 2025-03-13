6ac76626698df1f7dcb8ceb2e7c79f698335da23
haesa <xdhaesa@gmail.com>
2025-03-13 15:02:19 +0900

    #846 [FEAT] 파이어베이스 설정 및 포그라운드 메시지 수신

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
+
+import {
+  requestPermission,
+  onMessageListener,
+} from '@/feature/alert/lib/firebase-messaging';
 
 import { Navbar, Sidebar } from '@/shared/component';
 import { findRouteByPath } from '@/shared/lib';
 
 import { routeList } from '@/router.js';
-
 import styles from './App.module.css';
 
 function App() {
   const { pathname } = useLocation();
   const currentRoute = findRouteByPath(pathname, routeList);
   const hideNav = currentRoute?.meta?.hideNav ?? false;
+
+  useEffect(() => {
+    requestPermission(); // 앱 실행 시 푸시 알림 권한 요청
+
+    // 포그라운드 메시지 수신 이벤트 리스너 등록
+    onMessageListener()
+      .then((payload) => {
+        console.log('포그라운드 메시지 수신', payload);
+      })
+      .catch((err) => console.log('메시지 수신 오류:', err));
+  }, []);
 
   return (
     <div className={styles.app}>
---
+++ src/feature/alert/lib/firebase-messaging.js
@@ -0,0 +1,36 @@
+import { getToken, onMessage } from 'firebase/messaging';
+import { messaging } from './firebase-config';
+
+export const requestPermission = async () => {
+  console.log('알림 권한 요청 중...');
+  const permission = await Notification.requestPermission();
+
+  if (permission === 'granted') {
+    console.log('알림 권한 허용됨');
+
+    try {
+      const token = await getToken(messaging, {
+        vapidKey: process.env.REACT_APP_VAPID_KEY, // ✅ 크롬, 파이어폭스, 엣지에서 필요
+      });
+
+      console.log('FCM 토큰:', token);
+
+      // await fetch('/api/register-token', {
+      //   method: 'POST',
+      //   body: JSON.stringify({ token }),
+      //   headers: { 'Content-Type': 'application/json' },
+      // });
+    } catch (error) {
+      console.error('FCM 토큰 가져오기 실패:', error);
+    }
+  } else {
+    console.log('알림 권한 거부됨');
+  }
+};
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+  });
