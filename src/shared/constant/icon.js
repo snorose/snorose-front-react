d9894a5938af7fdd02bffd6cee7ca494c847b6f6
becherryu <81617058+becherryu@users.noreply.github.com>
2025-03-21 20:54:27 +0900

    #820 [FEAT] 공식 뱃지 아이콘 리스트 추가

--- src/shared/constant/icon.js
+++ src/shared/constant/icon.js
@@ -38,6 +38,7 @@ export const STATIC_ICON_LIST = Object.freeze({
   nestedArrow: 'nested-arrow',
   notificationRed: 'notification-red',
   noticeBell: 'notice-bell',
+  officialBadge: 'official-badge',
   point: 'point',
   pointCircle: 'point-circle',
   pencil: 'pencil',
