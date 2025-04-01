050a16556ae5f017af5920037584b21bcfc7b77a
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-04-02 00:01:30 +0900

    #854 [FEAT] onClick, onMouseOver, onMouseLeave 기능 추가
    호버 또는 클릭 시 아이콘이 바뀌는 기능을 추가했습니다.

--- src/feature/board/component/AttatchmentBar/AttatchmentBar.jsx
+++ src/feature/board/component/AttatchmentBar/AttatchmentBar.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './AttatchmentBar.module.css';
 import { Icon } from '@/shared/component';
 
 export default function AttatchmentBar() {
+  const [imageIconState, setImageIconState] = useState('image');
+  const [videoIconState, setVideoIconState] = useState('video');
+
   return (
     <div className={styles.bar}>
       <div className={styles.attatchmentBar}>
-        <Icon id='image' width={24} height={24} className={styles.picture} />
-        <Icon id='video' width={24} height={24}></Icon>
+        <Icon
+          id={imageIconState}
+          width={24}
+          height={24}
+          className={styles.image}
+          onClick={() => setImageIconState('image-fill')}
+          onMouseOver={() => setImageIconState('image-fill')}
+          onMouseLeave={() => setImageIconState('image')}
+        />
+        <Icon
+          id={videoIconState}
+          width={24}
+          height={24}
+          onClick={() => setVideoIconState('video-fill')}
+          onMouseOver={() => setVideoIconState('video-fill')}
+          onMouseLeave={() => setVideoIconState('video')}
+        ></Icon>
       </div>
     </div>
   );
