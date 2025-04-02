da34c6aa59b8e729c234b19c8e612826caf91764
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-04-02 19:23:21 +0900

    #854 [FEAT] Add Input Type File

--- src/feature/board/component/AttatchmentBar/AttatchmentBar.jsx
+++ src/feature/board/component/AttatchmentBar/AttatchmentBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import { useState } from 'react';
 import styles from './AttatchmentBar.module.css';
 import { Icon } from '@/shared/component';
 
 export default function AttatchmentBar() {
+  const img = useRef();
+  const vid = useRef();
   const [imageIconState, setImageIconState] = useState('image');
   const [videoIconState, setVideoIconState] = useState('video');
 
@@ -15,18 +17,36 @@ export default function AttatchmentBar() {
           width={24}
           height={24}
           className={styles.image}
-          onClick={() => setImageIconState('image-fill')}
+          onClick={() => {
+            setImageIconState('image-fill');
+            img.current.click();
+          }}
           onMouseOver={() => setImageIconState('image-fill')}
           onMouseLeave={() => setImageIconState('image')}
+        />
+        <input
+          type='file'
+          accept='image/*'
+          className={styles.imageInput}
+          ref={img}
         />
         <Icon
           id={videoIconState}
           width={24}
           height={24}
-          onClick={() => setVideoIconState('video-fill')}
+          onClick={() => {
+            setVideoIconState('video-fill');
+            vid.current.click();
+          }}
           onMouseOver={() => setVideoIconState('video-fill')}
           onMouseLeave={() => setVideoIconState('video')}
-        ></Icon>
+        />
+        <input
+          type='file'
+          accept='video/*'
+          className={styles.videoInput}
+          ref={vid}
+        />
       </div>
     </div>
   );
