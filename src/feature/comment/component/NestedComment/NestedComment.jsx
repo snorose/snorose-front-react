1311dac82da0a81c53155511387d540275b2d2ba
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-04-04 04:22:24 +0900

    #859 [STYLE] fill 아이콘과 stroke 아이콘 분리

--- src/feature/comment/component/NestedComment/NestedComment.jsx
+++ src/feature/comment/component/NestedComment/NestedComment.jsx
@@ -85,7 +85,7 @@ export default function NestedComment({
             onClick={() => (isLiked ? unlike.mutate() : like.mutate())}
           >
             <Icon
-              id='like'
+              id='like-fill'
               width={13}
               height={12}
               fill={isLiked ? '#5F86BF' : '#D9D9D9'}
