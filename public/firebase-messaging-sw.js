b5cfdbf740ab9b6d89691bfb7a8d2622b73a3e31
haesa <xdhaesa@gmail.com>
2025-04-08 17:52:49 +0900

    #846 [FEAT] FCM payload 되돌리기

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,38 +5,38 @@ importScripts(
   'https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js'
 );
 
-class CustomPushEvent extends Event {
-  constructor(data) {
-    super('push');
+// class CustomPushEvent extends Event {
+//   constructor(data) {
+//     super('push');
 
-    Object.assign(this, data);
-    this.custom = true;
-  }
-}
+//     Object.assign(this, data);
+//     this.custom = true;
+//   }
+// }
 
-self.addEventListener('push', (e) => {
-  if (e.custom) return;
+// self.addEventListener('push', (e) => {
+//   if (e.custom) return;
 
-  const oldData = e.data;
-  const newEvent = new CustomPushEvent({
-    data: {
-      ehheh: oldData.json(),
-      json() {
-        const newData = oldData.json();
-        newData.data = {
-          ...newData.data,
-          ...newData.notification,
-        };
-        delete newData.notification;
-        return newData;
-      },
-    },
-    waitUntil: e.waitUntil.bind(e),
-  });
+//   const oldData = e.data;
+//   const newEvent = new CustomPushEvent({
+//     data: {
+//       ehheh: oldData.json(),
+//       json() {
+//         const newData = oldData.json();
+//         newData.data = {
+//           ...newData.data,
+//           ...newData.notification,
+//         };
+//         delete newData.notification;
+//         return newData;
+//       },
+//     },
+//     waitUntil: e.waitUntil.bind(e),
+//   });
 
-  e.stopImmediatePropagation();
-  dispatchEvent(newEvent);
-});
+//   e.stopImmediatePropagation();
+//   dispatchEvent(newEvent);
+// });
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC6bVJTwYpW-ffJ2j2HSY45FwXdXRA545k',
@@ -49,11 +49,11 @@ const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging(app);
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(payload);
-  const notificationTitle = payload.data.title;
-  const notificationOptions = {
-    body: payload.data.body,
-    // icon: payload.icon
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  // console.log(payload);
+  // const notificationTitle = payload.data.title;
+  // const notificationOptions = {
+  //   body: payload.data.body,
+  //   // icon: payload.icon
+  // };
+  // self.registration.showNotification(notificationTitle, notificationOptions);
 });
