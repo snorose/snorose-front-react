6ae07814321ae284c3d2b5244bc3fa644b6f9d6e
haesa <xdhaesa@gmail.com>
2025-04-08 18:23:12 +0900

    #846 [FEAT] service worker 푸시 알림에 icon 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,39 +5,6 @@ importScripts(
   'https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js'
 );
 
-// class CustomPushEvent extends Event {
-//   constructor(data) {
-//     super('push');
-
-//     Object.assign(this, data);
-//     this.custom = true;
-//   }
-// }
-
-// self.addEventListener('push', (e) => {
-//   if (e.custom) return;
-
-//   const oldData = e.data;
-//   const newEvent = new CustomPushEvent({
-//     data: {
-//       ehheh: oldData.json(),
-//       json() {
-//         const newData = oldData.json();
-//         newData.data = {
-//           ...newData.data,
-//           ...newData.notification,
-//         };
-//         delete newData.notification;
-//         return newData;
-//       },
-//     },
-//     waitUntil: e.waitUntil.bind(e),
-//   });
-
-//   e.stopImmediatePropagation();
-//   dispatchEvent(newEvent);
-// });
-
 const firebaseConfig = {
   apiKey: 'AIzaSyC6bVJTwYpW-ffJ2j2HSY45FwXdXRA545k',
   projectId: 'snorose-c37c1',
@@ -50,10 +17,9 @@ const messaging = firebase.messaging(app);
 
 messaging.onBackgroundMessage((payload) => {
   console.log(payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    // icon: payload.icon
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    icon: '/logos/snoroseLogo120.png',
+  });
 });
