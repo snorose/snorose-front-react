d10ed6a9684a415a578fcea74c9d167d79c7f4a9
misung.dev <misung.dev@gmail.com>
2025-04-15 00:28:45 +0900

    #843 [FEAT] Filter 스토리북

---
+++ src/stories/Filter.stories.jsx
@@ -0,0 +1,49 @@
+import { Filter } from '../feature/exam/component';
+
+const meta = {
+  title: 'Component/Filter',
+  component: Filter,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Filter 컴포넌트는 드롭다운 방식으로 옵션을 선택할 수 있도록 하며, 선택된 옵션은 URL의 쿼리 파라미터에 반영됩니다. `filterKey`는 쿼리 파라미터의 키를 의미하고, `options`는 선택 가능한 옵션 목록입니다.',
+      },
+    },
+  },
+  argTypes: {
+    filterKey: {
+      control: 'text',
+      description: 'URL에 사용할 쿼리 파라미터 키 이름',
+    },
+    placeholder: {
+      control: 'text',
+      description: '선택 전 표시될 기본 텍스트',
+    },
+    options: {
+      control: 'object',
+      description: '옵션 배열 (id, name 포함)',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+const Template = (args) => <Filter {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  filterKey: 'category',
+  placeholder: '시험종류',
+  options: [
+    { id: 1, name: '중간고사' },
+    { id: 2, name: '기말고사' },
+  ],
+};
