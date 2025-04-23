c913d1d538489ff77060a3c3ef5473e071b59383
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-04-23 17:47:07 +0900

    #895 [STYLE] 게시판 카테고리 UI 변경(파란 계열 디자인)

--- src/feature/board/component/BoardBar/BoardBar.jsx
+++ src/feature/board/component/BoardBar/BoardBar.jsx
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 import styles from './BoardBar.module.css';
 
 export default function BoardBar({ data }) {
+  const isOfficial = [60, 61, 62].includes(data.id);
+
   return (
-    <Link to={`/board/${data.textId}`}>
-      <div
-        className={styles.container}
+    <Link to={`/board/${data.textId}`} className={styles.container}>
+      <img
+        className={styles.image}
+        src={data.image}
+        alt={data.textId}
         style={{
-          backgroundImage: `url(${data.image})`,
+          height: isOfficial ? 'none' : '100%',
+          bottom: isOfficial ? '0.8rem' : '-0.2rem',
         }}
-      >
-        <div className={styles.textBox}>
-          <div className={styles.title}>{data.title}</div>
-          <div className={styles.description}>{data.desc}</div>
-        </div>
+      />
+      <div className={styles.textBox}>
+        <h1 className={styles.title}>{data.title}</h1>
+        <p className={styles.description}>{data.desc}</p>
       </div>
     </Link>
   );
