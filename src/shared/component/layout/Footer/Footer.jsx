b7b4d6e51c20db9fd9e0bfad6d6645b497aef2df
haesa <xdhaesa@gmail.com>
2025-05-12 21:01:31 +0900

    #927 [STYLE] Footer 컴포넌트 UI 변경사항 적용

--- src/shared/component/layout/Footer/Footer.jsx
+++ src/shared/component/layout/Footer/Footer.jsx
@@ -2,43 +2,48 @@ import { Link } from 'react-router-dom';
 
 import { Icon } from '@/shared/component';
 
-import styles from './Footer.module.css';
+import style from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <Icon id='logo' width={109} height={20} />
-      <div className={styles.info}>
+    <footer className={style.footer}>
+      <Icon id='logo' width={118} height={21} />
+
+      <div className={style.info}>
         <p>
-          {'CONTACT US : '}
-          <a href='mailto:smsnorose@gmail.com'>snorose1906@gmail.com</a>
+          <span className={style.bold}>CONTACT US </span>
+          <a href='mailto:snorose1906@gmail.com'>snorose1906@gmail.com</a>
         </p>
-        <p>후원하기 : 카카오뱅크 3333-31-8162062 (예금주: 김*지)</p>
-        <div className={styles.menu}>
-          <Link to='/my-page/service-policy' className={styles.link}>
-            <span>서비스 이용 약관</span>
-          </Link>
-          {' | '}
-          <Link
-            to='https://www.notion.so/snorose/1147ef0aa3bf8039add2e00a4edd0eb4'
-            target='_blank'
-            className={styles.link}
-          >
-            <span>배너 및 광고 문의</span>
-          </Link>{' '}
-          {' | '}
-          <Link to='/my-page/privacy-policy' className={styles.link}>
-            <span>개인정보 처리방침</span>
-          </Link>{' '}
-          {' | '}
-          <Link
-            to='https://www.instagram.com/snorose1906/'
-            target='_blank'
-            className={styles.link}
-          >
-            <Icon id='instagram' width={16.5} height={16.5} />
-          </Link>
-        </div>
+        <p>
+          <span className={style.bold}>숙명여대 후원하기</span> 카카오뱅크
+          3333-31-8162062 (예금주: 김*지)
+        </p>
+      </div>
+
+      <div className={style.menu}>
+        <Link to='/my-page/service-policy' className={style.link}>
+          서비스 이용 약관
+        </Link>
+        {' | '}
+        <Link
+          to='https://www.notion.so/snorose/1147ef0aa3bf8039add2e00a4edd0eb4'
+          target='_blank'
+          className={style.link}
+        >
+          배너 및 광고 문의
+        </Link>{' '}
+        {' | '}
+        <Link to='/my-page/privacy-policy' className={style.link}>
+          개인정보 처리방침
+        </Link>{' '}
+        {' | '}
+        <Link
+          to='https://www.instagram.com/snorose1906/'
+          target='_blank'
+          className={style.link}
+        >
+          <Icon id='instagram' width={16.5} height={16.5} />
+        </Link>
       </div>
     </footer>
   );
