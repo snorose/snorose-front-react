f73f2a591b5ffef0bce4976532540ab1312eab1a
becherryu <81617058+becherryu@users.noreply.github.com>
2025-05-21 15:35:20 +0900

    #991 [FEAT] 서버 점검용 Maintenance 페이지 구성

---
+++ src/page/maintenance/MaintenancePage/MaintenancePage.jsx
@@ -0,0 +1,27 @@
+import {
+  MAINTENANCE_START,
+  MAINTENANCE_END,
+  useMaintenance,
+} from '@/feature/maintenance/hook/useMaintenance';
+
+import MaintenanceConfig from '@/assets/images/maintenanceConfig.svg';
+import { Icon } from '@/shared/component';
+
+import styles from './MaintenancePage.module.css';
+
+export default function MaintenancePage() {
+  const period = useMaintenance(MAINTENANCE_START, MAINTENANCE_END);
+  return (
+    <div className={styles.main}>
+      <img src={MaintenanceConfig} alt='서버 점검' />
+      <h1 className={styles.config}>서버 점검 안내</h1>
+      <div className={styles.text}>
+        <p>안정적인 서비스 제공을 위한 서버 점검을 하고 있습니다.</p>
+        <p className={styles.date}>일시 : {period}</p>
+
+        <p>이용에 불편을 드리게 된 점 양해 부탁드립니다.</p>
+      </div>
+      <Icon className={styles.logo} id='logo' width={221} height={25} />
+    </div>
+  );
+}
