70f52c0eadac9583b87e84cc59373c0ba3501d0b
becherryu <81617058+becherryu@users.noreply.github.com>
2025-05-21 11:21:41 +0900

    #986 [STYLE] about 스노로즈 변경된 아코디언 UI 적용

--- src/feature/home/component/Accordion/Accordion.jsx
+++ src/feature/home/component/Accordion/Accordion.jsx
@@ -1,25 +1,41 @@
-import { useState } from 'react';
-
-import { Icon } from '@/shared/component';
+import { Icon, PrimaryButton } from '@/shared/component';
 
 import styles from './Accordion.module.css';
 
-export default function Accordion({ title, children }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen((prev) => !prev);
+export default function Accordion({ title, children, isOpen, onClick }) {
+  const goApply = () => {
+    window.location.href =
+      'https://www.notion.so/snorose/2024-2-10c7ef0aa3bf8027a04ee35b7c521e12';
+  };
 
   return (
     <div className={`${styles.accordion} ${isOpen && styles.opend}`}>
-      <div className={styles.panelHeader} onClick={toggle}>
+      <div
+        className={`${styles.panelHeader} ${title === '스노로즈 소개' ? styles.roundTop : ''} ${title === '명예의 전당' ? styles.roundBottom : ''} ${isOpen && styles.opend}`}
+        onClick={onClick}
+      >
         <span>{title}</span>
         <Icon
           className={styles.toggleIcon}
-          id='blue-triangle'
+          id='angle-down-blue'
           width={20}
           height={20}
         />
       </div>
-      <div className={styles.panelBody}>{children}</div>
+      <div
+        className={`${styles.panelBody} ${title === '명예의 전당' ? styles.roundBottom : ''}`}
+      >
+        {children}
+      </div>
+      {title === '명예의 전당' && isOpen && (
+        <PrimaryButton
+          className={styles.apply}
+          // onClick={() => alert('지원 기간이 아닙니다!')}
+          onClick={goApply}
+        >
+          리자 지원하기
+        </PrimaryButton>
+      )}
     </div>
   );
 }
