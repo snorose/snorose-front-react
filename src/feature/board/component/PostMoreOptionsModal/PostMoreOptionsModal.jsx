92b0f6214d4ee600331e4a7050b89cbe369956b5
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-05-28 15:25:48 +0900

    #1010 [REFACTOR] 게시글 더보기 클릭 시 나타나는 모달 -> 컴포넌트로 분리 및 새 디자인 적용

---
+++ src/feature/board/component/PostMoreOptionsModal/PostMoreOptionsModal.jsx
@@ -0,0 +1,46 @@
+import { Icon, Portal } from '@/shared/component';
+import styles from './PostMoreOptionsModal.module.css';
+
+export default function PostMoreOptionsModal({ modal, setModal }) {
+  if (!modal.id) {
+    return null;
+  }
+  return (
+    <Portal portalKey='modal'>
+      <div
+        className={styles.background}
+        onClick={() => setModal({ id: null, reportType: null })}
+      >
+        <div className={styles.container} onClick={(e) => e.stopPropagation()}>
+          <h3 className={styles.title}>게시글</h3>
+          <ul className={styles.content}>
+            <li
+              className={styles.contentItem}
+              onClick={() => setModal({ id: 'report-post', reportType: null })}
+            >
+              <p>게시글 신고하기</p>
+              <Icon
+                id='report-post'
+                className={styles.itemIcon}
+                width={19}
+                height={22}
+              />
+            </li>
+            <li
+              className={styles.contentItem}
+              onClick={() => setModal({ id: 'report-user', reportType: null })}
+            >
+              <p>이용자 신고하기</p>
+              <Icon
+                id='report-user'
+                className={styles.itemIcon}
+                width={17}
+                height={17}
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Portal>
+  );
+}
