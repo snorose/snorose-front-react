f025eac2ecef8fd47881f436035ce3ec309fbccf
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-05-28 15:27:36 +0900

    #1010 [REFACTOR] 게시글/이용자 신고 상세 옵션 모달  -> 컴포넌트로 분리 및 새 디자인 적용

---
+++ src/feature/board/constant/reportUserReasonList.js
@@ -0,0 +1,37 @@
+export const REPORT_USER_REASON_LIST = [
+  {
+    reportType: 'USER_IMPERSONATION',
+    iconId: 'user-question-mark',
+    label: '타인 사칭',
+    width: 20,
+    height: 18,
+  },
+  {
+    reportType: 'USER_FRAUD',
+    iconId: 'dollar-circle',
+    label: '사기',
+    width: 20,
+    height: 20,
+  },
+  {
+    reportType: 'USER_EXTERNAL_PARTY',
+    iconId: 'question-mark-circle',
+    label: '외부인 의심',
+    width: 20,
+    height: 20,
+  },
+  {
+    reportType: 'USER_HARASSMENT',
+    iconId: 'skull-face',
+    label: '괴롭힘 / 사이버폭력',
+    width: 20,
+    height: 20,
+  },
+  {
+    reportType: 'USER_OTHER',
+    iconId: 'null',
+    label: '기타',
+    width: 20,
+    height: 20,
+  },
+];
