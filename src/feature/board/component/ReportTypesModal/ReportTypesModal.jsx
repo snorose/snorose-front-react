9e975f4840b736f587d347f098c19d0dd883c3e3
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-05-28 15:48:07 +0900

    #1010 [REFACTOR] 변수명, 파일명 수정

--- src/feature/board/component/ReportTypesModal/ReportTypesModal.jsx
+++ src/feature/board/component/ReportTypesModal/ReportTypesModal.jsx
@@ -1,19 +1,19 @@
 import { DimModal, Icon } from '@/shared/component';
-import { REPORT_COMMENT_REASON_LIST } from '../../constant/reportCommentReasonList';
-import { REPORT_POST_REASON_LIST } from '../../constant/reportPostReasonList';
-import { REPORT_USER_REASON_LIST } from '../../constant/reportUserReasonList';
+import { REPORT_COMMENT_TYPE_LIST } from '../../constant/reportCommentTypeList';
+import { REPORT_POST_TYPE_LIST } from '../../constant/reportPostTypeList';
+import { REPORT_USER_TYPE_LIST } from '../../constant/reportUserTypeList';
 import styles from './ReportTypesModal.module.css';
 
 export default function ReportTypesModal({ modal, setModal }) {
   console.log(modal);
   const getReportData = () => {
     switch (modal.id) {
-      case 'report-post':
-        return { list: REPORT_POST_REASON_LIST, label: '게시글' };
-      case 'report-user':
-        return { list: REPORT_USER_REASON_LIST, label: '이용자' };
-      case 'report-comment':
-        return { list: REPORT_COMMENT_REASON_LIST, label: '댓글' };
+      case 'report-post-types':
+        return { list: REPORT_POST_TYPE_LIST, label: '게시글' };
+      case 'report-user-types':
+        return { list: REPORT_USER_TYPE_LIST, label: '이용자' };
+      case 'report-comment-types':
+        return { list: REPORT_COMMENT_TYPE_LIST, label: '댓글' };
       default:
         return { list: [], label: '' };
     }
