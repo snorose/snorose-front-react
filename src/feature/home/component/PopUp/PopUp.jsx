0de644fe4b3c8628ebd2a5ff1d9836f80b98ffbf
류미성 <misung.dev@gmail.com>
2025-06-11 00:56:05 +0900

    #1040 [FEAT] 팝업 수정 (#1041)

--- src/feature/home/component/PopUp/PopUp.jsx
+++ src/feature/home/component/PopUp/PopUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { usePopUp } from '@/feature/home/hook';
-import calendar from '@/feature/home/component/PopUp/june-calendar.png';
+import accountingReport from '@/feature/home/component/PopUp/accountingReport.png';
 import styles from './PopUp.module.css';
 
 const content = (
@@ -11,50 +11,62 @@ const content = (
         ❄️ 안녕하세요. 숙명인을 위한 커뮤니티, 스노로즈입니다. ❄️
       </p>
 
-      <Heading>1. 1차 후원금 모금 이벤트 마감 안내 (1,027,852원)</Heading>
+      <Heading>
+        1. 포인트 미지급 일시적 해제 오류에 따른 포인트 회수 조치 안내
+      </Heading>
 
       <Content>
-        - 5월 31일 기준 목표 금액 100만원을 달성하며 후원금 모금 이벤트가
-        성공적으로 종료되었습니다. 참여해주신 모든 숙명인분들께 진심으로
-        감사드립니다💙
-      </Content>
-
-      <Heading>2. 6월 스노로즈 캘린더</Heading>
-
-      <Content>
-        <img src={calendar} alt='6월 달력' />
-      </Content>
-
-      <Heading>3. 글/댓글 포인트 미지급 기간 안내</Heading>
-
-      <Content>
-        <strong>기간: 6월 3일(화) 00:00 ~ 6월 23일(월) 23:59</strong> <br />- 위
-        기간 동안 글, 댓글 포인트는 지급되지 않습니다. <br />- 자세한 내용은{' '}
+        - 포인트 미지급 기간인 6월 7일 0시 ~ 6월 8일 20시 사이 시스템 오류로
+        잘못 지급된 글/댓글 포인트가 모두 회수되었습니다. <br />- 자세한 내용은{' '}
         <a
           className={styles.link}
-          href='https://www.snorose.com/board/notice/post/1833145'
+          href='https://www.snorose.com/board/notice/post/1835262'
           alt='공지'
         >
-          관련 공지사항
+          [관련 공지]
         </a>
-        를 참조 부탁드립니다.
+        를 참고 부탁드립니다.
       </Content>
 
-      <Heading>4. 스노로즈 블로그 회계편 공개</Heading>
+      <Heading>
+        2. iOS 일부 기기 및 브라우저에서 스노로즈 접속 불가 현상 안내
+      </Heading>
 
       <Content>
-        <strong>&lt;스노로즈 리뉴얼&gt; 회계편</strong>이 업로드되었습니다.
-        <br />
-        지금{' '}
+        - 현재 iOS 기기에서 네이버앱, 구글앱, 인스타그램 앱을 통한 스노로즈
+        접속이 제한되는 현상이 발생하고 있습니다. <br />- 보다 원활한 이용을
+        위해 크롬 또는 사파리 브라우저를 통해 접속해주시길 부탁드립니다.
+        <br />- 자세한 내용은{' '}
         <a
           className={styles.link}
-          href='https://snorose.notion.site/1a37ef0aa3bf8071bcd0cb35c035636e'
-          alt='블로그'
+          href='https://www.snorose.com/board/notice/post/1835263'
+          alt='공지'
         >
-          스노로즈 블로그
+          [관련 공지]
         </a>
-        에서 확인해보세요!
-        <br />
+        를 참고 바랍니다.
+      </Content>
+
+      <Heading>3. 5월 회계 보고</Heading>
+      <Content>
+        -{' '}
+        <a
+          className={styles.link}
+          href='https://docs.google.com/spreadsheets/u/0/d/1MzXAHemKqPwVj3PzPJch5cR5z49gFtzABPgJ_FVjCXM/edit?usp=sharing&pli=1&authuser=0'
+          alt='회계 보고 링크'
+        >
+          [스노로즈 회계 보고 링크 바로가기]
+        </a>
+        <br />- 자세한 내용은{' '}
+        <a
+          className={styles.link}
+          href='https://www.snorose.com/board/notice/post/1835239'
+          alt='공지'
+        >
+          [관련 공지]
+        </a>
+        를 참고 부탁드립니다.
+        <img src={accountingReport} alt='5월 회계보고' />
       </Content>
 
       <hr />
