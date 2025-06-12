20d47adfe106877724f7d40b5a09777181caee24
haesa <34948133+haesa@users.noreply.github.com>
2025-06-12 15:40:05 +0900

    #883 [CHORE] 시험후기에서 사용하지 않는 category, content 데이터 삭제 (#1005)

--- src/page/exam/ExamReviewPage/ExamReviewPage.jsx
+++ src/page/exam/ExamReviewPage/ExamReviewPage.jsx
@@ -172,7 +172,6 @@ export default function ExamReviewPage() {
   }
 
   const {
-    content,
     commentCount,
     createdAt,
     examType,
@@ -248,7 +247,6 @@ export default function ExamReviewPage() {
             align={FLEX_ALIGN.flexStart}
           />
         </div>
-        {content && <div className={styles.content}>{content}</div>}
         <ReviewDownload
           className={styles.fileDownload}
           fileName={fileName}
