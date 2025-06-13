10b2d28a29f638e5a85f5bdfea907151ebdf4d1b
becherryu <81617058+becherryu@users.noreply.github.com>
2025-06-13 20:59:36 +0900

    #986 [STYLE] 아코디언 함수 분리

--- src/feature/home/component/Accordion/Accordion.jsx
+++ src/feature/home/component/Accordion/Accordion.jsx
@@ -2,31 +2,24 @@ import { Icon, PrimaryButton } from '@/shared/component';
 
 import styles from './Accordion.module.css';
 
-export default function Accordion({ title, children, isOpen, onClick }) {
-  const goApply = () => {
-    window.location.href =
-      'https://www.notion.so/snorose/2024-2-10c7ef0aa3bf8027a04ee35b7c521e12';
-  };
+const goApply = () => {
+  window.location.href =
+    'https://www.notion.so/snorose/2024-2-10c7ef0aa3bf8027a04ee35b7c521e12';
+};
 
+export default function Accordion({ title, children, isOpen, onClick }) {
   return (
     <div className={`${styles.accordion} ${isOpen && styles.opend}`}>
-      <div
-        className={`${styles.panelHeader} ${title === '스노로즈 소개' ? styles.roundTop : ''} ${title === '명예의 전당' ? styles.roundBottom : ''} ${isOpen && styles.opend}`}
-        onClick={onClick}
-      >
+      <div className={`${styles.panelHeader}`} onClick={onClick}>
         <span>{title}</span>
         <Icon
           className={styles.toggleIcon}
           id='angle-down-blue'
-          width={20}
-          height={20}
+          width={16}
+          height={16}
         />
       </div>
-      <div
-        className={`${styles.panelBody} ${title === '명예의 전당' ? styles.roundBottom : ''}`}
-      >
-        {children}
-      </div>
+      <div className={`${styles.panelBody}`}>{children}</div>
       {title === '명예의 전당' && isOpen && (
         <PrimaryButton
           className={styles.apply}
