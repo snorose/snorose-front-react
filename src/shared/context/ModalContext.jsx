ab6be2e93c4f66406065492c8e8e8a98ef2d89dc
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-06-15 17:43:27 +0900

    #1010 [REFACTOR] ModalContext 기존 버전으로 되돌리기

--- src/shared/context/ModalContext.jsx
+++ src/shared/context/ModalContext.jsx
@@ -1,53 +1,16 @@
 import { createContext, useState } from 'react';
 
-import ConfirmModal from '@/shared/component/modal/ConfirmModal';
-import { MoreOptionModal, OptionModal } from '../component';
-
 export const ModalContext = createContext();
 
 export function ModalProvider({ children }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [type, setType] = useState('');
-  const [modalProps, setModalProps] = useState({
-    title: '',
-    description: '',
-    primaryText: '',
-    secondaryText: '',
-    onPrimary: () => {},
-    onSecondary: () => {},
-    functionList: [],
+  const [modal, setModal] = useState({
+    id: null,
+    type: null,
   });
 
-  const open = (type, modalProps) => {
-    setType(type);
-    setIsOpen(true);
-
-    setModalProps({
-      ...modalProps,
-      onPrimary: () => {
-        if (modalProps.onPrimary) {
-          modalProps.onPrimary();
-        }
-
-        setIsOpen(false);
-      },
-      onSecondary: () => {
-        if (modalProps.onSecondary) {
-          modalProps.onSecondary();
-        }
-
-        setIsOpen(false);
-      },
-    });
-  };
-
   return (
-    <ModalContext.Provider value={{ open }}>
+    <ModalContext.Provider value={{ modal, setModal }}>
       {children}
-
-      {type === 'confirm' && isOpen && <ConfirmModal {...modalProps} />}
-      {type === 'option' && isOpen && <OptionModal {...modalProps} />}
-      {type === 'more-option' && isOpen && <MoreOptionModal {...modalProps} />}
     </ModalContext.Provider>
   );
 }
