281777844a2de839f5ed5a9047b1379ac904bae0
류미성 <misung.dev@gmail.com>
2025-06-24 22:31:28 +0900

    #1048 [FEAT] 팝업 재수정 (#1049)

--- src/feature/home/component/PopUp/PopUp.jsx
+++ src/feature/home/component/PopUp/PopUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { usePopUp } from '@/feature/home/hook';
-import accountingReport from '@/feature/home/component/PopUp/accountingReport.png';
+import likeRewardsTable from '@/feature/home/component/PopUp/like-rewards.png';
 import styles from './PopUp.module.css';
 
 const content = (
@@ -11,8 +11,7 @@ const content = (
         ❄️ 안녕하세요. 숙명인을 위한 커뮤니티, 스노로즈입니다. ❄️
       </p> */}
 
-      <Heading>❄️ '좋아요' 포인트 지급 기준 변경 안내 ❄️</Heading>
-
+      <Heading>['좋아요' 포인트 지급 기준 변경 안내]</Heading>
       <Content>
         스노로즈 내 <strong>유익한 글 작성</strong>과{' '}
         <strong>건강한 소통</strong>을 활성화하기 위해,
@@ -26,31 +25,19 @@ const content = (
         앞으로도 스노로즈에 많은 관심과 활발한 참여 부탁드립니다.
         <br />
         <br />
-        <strong>✅ 기존 지급 기준</strong>
-        <br />
-        - 좋아요 10개: 5포인트
-        <br />
-        - 좋아요 100개: 10포인트 추가 지급(총 15포인트)
-        <br />
-        - 좋아요 1000개: 100포인트 추가 지급(총 115포인트)
-        <br />
-        <br />
-        <strong>✅ 변경된 지급 기준 (누적 기준)</strong>
-        <br />
-        - 좋아요 5개: 5포인트
-        <br />
-        - 좋아요 10개: 5포인트 추가 (총 10포인트)
-        <br />
-        - 좋아요 20개: 5포인트 추가 (총 15포인트)
-        <br />
-        - 좋아요 50개: 20포인트 추가 (총 35포인트)
-        <br />
-        - 좋아요 100개: 30포인트 추가 (총 65포인트)
-        <br />
-        - 좋아요 1000개: 100포인트 추가 (총 165포인트)
-        <br />
+        <img src={likeRewardsTable} alt='좋아요 수 리워드 변경' />
         <br />※ 좋아요 10개 달성 시, 베숙트 등재 및 하루 최대 3회 지급 조건은
         기존과 동일하게 유지됩니다.
+        <br />
+        <br />- 자세한 내용은{' '}
+        <a
+          className={styles.link}
+          href='https://snorose.com/board/notice/post/1835541'
+          alt='공지'
+        >
+          [관련 공지]
+        </a>
+        를 참고해주시길 바랍니다.
       </Content>
 
       {/* <Heading>
@@ -72,27 +59,7 @@ const content = (
         를 참고 바랍니다.
       </Content>
 
-      <Heading>3. 5월 회계 보고</Heading>
-      <Content>
-        -{' '}
-        <a
-          className={styles.link}
-          href='https://docs.google.com/spreadsheets/u/0/d/1MzXAHemKqPwVj3PzPJch5cR5z49gFtzABPgJ_FVjCXM/edit?usp=sharing&pli=1&authuser=0'
-          alt='회계 보고 링크'
-        >
-          [스노로즈 회계 보고 링크 바로가기]
-        </a>
-        <br />- 자세한 내용은{' '}
-        <a
-          className={styles.link}
-          href='https://www.snorose.com/board/notice/post/1835239'
-          alt='공지'
-        >
-          [관련 공지]
-        </a>
-        를 참고 부탁드립니다.
-        <img src={accountingReport} alt='5월 회계보고' />
-      </Content> */}
+     */}
 
       <hr />
       <Content>
