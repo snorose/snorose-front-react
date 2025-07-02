98933d11d7c9f10b2ee37e29e77d8352870918e1
dawncoding <sunnuri99@gmail.com>
2025-07-02 17:51:13 +0900

    #1047 [REFACTOR] 불필요한 로직 제거

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import { useFeatureIsOn } from '@growthbook/growthbook-react';
 
 import { useAuth } from '@/shared/hook';
 import { Navbar, Sidebar } from '@/shared/component';
-import {
-  detectDeviceInfo,
-  detectInAppBrowser,
-  findRouteByPath,
-} from '@/shared/lib';
+import { findRouteByPath } from '@/shared/lib';
 
 import { PushNotificationManager } from '@/feature/alert/lib';
 
@@ -25,7 +21,7 @@ import { MaintenancePage } from './page/maintenance';
 
 function App() {
   const isEnabled = useFeatureIsOn('push-notification');
-  const { pathname, key: locationKey } = useLocation();
+  const { pathname } = useLocation();
   const currentRoute = findRouteByPath(pathname, routeList);
   const hideNav = currentRoute?.meta?.hideNav ?? false;
   const { status } = useAuth();
@@ -37,23 +33,6 @@ function App() {
       PushNotificationManager.listenForegroundMessage();
     }
   }, [isEnabled, status]);
-
-  // 인앱 브라우저 감지
-  // useEffect(() => {
-  //   const { platform } = detectDeviceInfo();
-  //   const isInApp = detectInAppBrowser();
-
-  //   if (platform === 'iOS' && isInApp) {
-  //     const url = window.location.href;
-  //     alert(
-  //       '현재 인앱 브라우저에서는 일부 기능이 제한될 수 있습니다.\n사파리나 크롬에서 다시 열어주세요.\n주소가 클립보드에 복사되었습니다.'
-  //     );
-
-  //     if (navigator.clipboard) {
-  //       navigator.clipboard.writeText(url).catch(() => {});
-  //     }
-  //   }
-  // }, [locationKey]);
 
   // 서버 점검 페이지 처리
   const now = new Date();
