8b7f9b83bd800f249d846cded335e519754f04ae
dawncoding <sunnuri99@gmail.com>
2025-07-02 17:36:19 +0900

    #1047 [REFACTOR] 브라우저의 firebase messaging의 지원 여부 확인 후 getMessaging 실행 로직 추가

--- src/feature/alert/lib/firebase-config.js
+++ src/feature/alert/lib/firebase-config.js
@@ -1,18 +1,27 @@
-// import { initializeApp } from 'firebase/app';
-// import { getMessaging } from 'firebase/messaging';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, isSupported } from 'firebase/messaging';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-// };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
-// // Firebase 초기화
-// const firebaseApp = initializeApp(firebaseConfig);
-// const messaging = getMessaging(firebaseApp);
+// Firebase 초기화
+const firebaseApp = initializeApp(firebaseConfig);
 
-// export { messaging };
+let messaging = null;
+
+isSupported().then((supported) => {
+  if (supported) {
+    messaging = getMessaging(firebaseApp);
+  } else {
+    console.warn('이 브라우저에서 Firebase Messaging을 지원하지 않습니다.');
+  }
+});
+
+export { messaging };
