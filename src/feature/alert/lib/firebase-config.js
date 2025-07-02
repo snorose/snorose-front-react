f357f166b80731d9802b685655d38e590b8fe151
dawncoding <sunnuri99@gmail.com>
2025-07-02 17:30:19 +0900

    #1047 [CHORE] getMessaging 호출 주석 처리

--- src/feature/alert/lib/firebase-config.js
+++ src/feature/alert/lib/firebase-config.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
+// import { initializeApp } from 'firebase/app';
+// import { getMessaging } from 'firebase/messaging';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+// };
 
-// Firebase 초기화
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
+// // Firebase 초기화
+// const firebaseApp = initializeApp(firebaseConfig);
+// const messaging = getMessaging(firebaseApp);
 
-export { messaging };
+// export { messaging };
