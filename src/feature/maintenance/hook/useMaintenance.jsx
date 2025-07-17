ab88d1a71678cb94485c44a229db5d0246729059
becherryu <81617058+becherryu@users.noreply.github.com>
2025-07-17 16:35:00 +0900

    #1050 [CHORE] 250718 서버 점검 시간 반영

--- src/feature/maintenance/hook/useMaintenance.jsx
+++ src/feature/maintenance/hook/useMaintenance.jsx
@@ -1,7 +1,7 @@
 import { dateStrWithDay, serverTimeStr } from '@/shared/lib';
 
-export const MAINTENANCE_START = new Date('2025-07-17T01:00:00+09:00'); // 서버 점검 시작(년-월-일THH:MM:SS+한국기준)
-export const MAINTENANCE_END = new Date('2025-07-18T19:00:00+09:00'); // 서버 점검 끝
+export const MAINTENANCE_START = new Date('2025-07-18T08:00:00+09:00'); // 서버 점검 시작(년-월-일THH:MM:SS+한국기준)
+export const MAINTENANCE_END = new Date('2025-07-18T16:00:00+09:00'); // 서버 점검 끝
 
 export function useMaintenance(MAINTENANCE_START, MAINTENANCE_END) {
   const isSameDate =
