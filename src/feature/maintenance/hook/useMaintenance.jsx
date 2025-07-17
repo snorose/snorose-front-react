17651f886a81d94cea3c7b9e0217e589e92baf96
becherryu <81617058+becherryu@users.noreply.github.com>
2025-07-17 16:30:00 +0900

    #1050 [MODIFY] 시간 변환 lib으로 이동 및 날짜 출력 형식 변경

--- src/feature/maintenance/hook/useMaintenance.jsx
+++ src/feature/maintenance/hook/useMaintenance.jsx
@@ -1,35 +1,16 @@
-export const MAINTENANCE_START = new Date('2025-05-06T01:00:00+09:00'); // 서버 점검 시작(년-월-일THH:MM:SS+한국기준)
-export const MAINTENANCE_END = new Date('2025-05-06T19:00:00+09:00'); // 서버 점검 끝
+import { dateStrWithDay, serverTimeStr } from '@/shared/lib';
+
+export const MAINTENANCE_START = new Date('2025-07-17T01:00:00+09:00'); // 서버 점검 시작(년-월-일THH:MM:SS+한국기준)
+export const MAINTENANCE_END = new Date('2025-07-18T19:00:00+09:00'); // 서버 점검 끝
 
 export function useMaintenance(MAINTENANCE_START, MAINTENANCE_END) {
   const isSameDate =
     MAINTENANCE_START.toDateString() === MAINTENANCE_END.toDateString();
 
-  const days = [
-    '일요일',
-    '월요일',
-    '화요일',
-    '수요일',
-    '목요일',
-    '금요일',
-    '토요일',
-  ];
-
-  const startDateStr = `${MAINTENANCE_START.getFullYear()}.${String(MAINTENANCE_START.getMonth() + 1).padStart(2, '0')}.${String(MAINTENANCE_START.getDate()).padStart(2, '0')} ${days[MAINTENANCE_START.getDay()]}`;
-
-  const endDateStr = `${MAINTENANCE_END.getFullYear()}.${String(MAINTENANCE_END.getMonth() + 1).padStart(2, '0')}.${String(MAINTENANCE_END.getDate()).padStart(2, '0')} ${days[MAINTENANCE_END.getDay()]}`;
-
-  const startTime = MAINTENANCE_START.toLocaleTimeString('ko-KR', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-  });
-
-  const endTime = MAINTENANCE_END.toLocaleTimeString('ko-KR', {
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-  });
+  const startDateStr = dateStrWithDay(MAINTENANCE_START);
+  const endDateStr = dateStrWithDay(MAINTENANCE_END);
+  const startTime = serverTimeStr(MAINTENANCE_START);
+  const endTime = serverTimeStr(MAINTENANCE_END);
 
   // YYYY/MM/DD (Day) HH:MM ~ HH:MM
   return isSameDate
