8d276c1cb9f917918ba70bd6128f360b99bd4d48
misung.dev <misung.dev@gmail.com>
2025-07-23 21:08:00 +0900

    #1092 [REFACTOR] footer 상수값으로 분리

--- src/shared/constant/menus.js
+++ src/shared/constant/menus.js
@@ -94,6 +94,21 @@ export const SIDEBAR_MENUS = Object.freeze([
   },
 ]);
 
+export const FOOTER_MENUS = Object.freeze([
+  {
+    to: '/my-page/service-policy',
+    title: '서비스 이용 약관',
+  },
+  {
+    to: '/my-page/privacy-policy',
+    title: '개인정보 처리방침',
+  },
+  {
+    to: 'https://www.notion.so/snorose/1147ef0aa3bf8039add2e00a4edd0eb4',
+    title: '배너 및 광고 문의',
+  },
+]);
+
 export const NOT_LOGIN_MENUS = Object.freeze([
   '스노로즈',
   '문의/신고',
