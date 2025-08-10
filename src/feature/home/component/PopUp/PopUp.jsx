560e5745f40031f2e462bfa24e8a3e2f24057636
misung-dev <misung.dev@gmail.com>
2025-08-10 16:46:15 +0900

    #1111 [SNO-163] 0810 팝업 공지 수정 (#1112)
    * #111 [FEAT] 0808 팝업 - 블로그로 내용 수정
    
    * #1111 [FEAT] 팝업에 회계보고 내용 추가

--- src/feature/home/component/PopUp/PopUp.jsx
+++ src/feature/home/component/PopUp/PopUp.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { usePopUp } from '@/feature/home/hook';
 import styles from './PopUp.module.css';
 import august from './august.jpeg';
+import accounting from './accounting.png';
 
 const content = (
   <div className={styles.notice}>
@@ -10,24 +11,54 @@ const content = (
       <p className={styles.hello}>
         ❄️ 안녕하세요. 숙명인을 위한 커뮤니티, 스노로즈입니다. ❄️
       </p>
-      {/* <Heading>1. 스노로즈 블로그 운영관리편 공개</Heading>
+      <Heading>
+        1. 스노로즈 블로그 - 무엇이든 물어보세요(무물) 특별편 공개
+      </Heading>
       <Content>
-        <strong>&lt;스노로즈 리뉴얼&gt; 운영관리편</strong>이 업로드되었습니다.
+        <strong>&lt;스노로즈 리뉴얼&gt; 무물 특별편</strong>이 업로드되었습니다.
         <br />
         지금{' '}
-        <a
-          className={styles.link}
-          href='https://snorose.notion.site/1e57ef0aa3bf80809d79c8ab544301b4?v=1e57ef0aa3bf81daa299000c4e65257c'
-          alt='스노로즈 블로그'
-        >
-          스노로즈 블로그
-        </a>
+        <strong>
+          <a
+            className={styles.link}
+            href='https://snorose.notion.site/2467ef0aa3bf806e9920dc5586292898'
+            alt='스노로즈 블로그'
+          >
+            스노로즈 블로그
+          </a>
+        </strong>
         에서 확인해보세요!
-      </Content> */}
+      </Content>
 
-      <Heading>1. 8월 스노로즈 캘린더</Heading>
+      <Heading>2. 7월 회계 보고</Heading>
       <Content>
-        {/* 스노로즈에 대한 학우 여러분의 <strong>궁금증</strong>을 풀어드릴 수 있는
+        -{' '}
+        <strong>
+          <a
+            className={styles.link}
+            href='https://docs.google.com/spreadsheets/u/0/d/1MzXAHemKqPwVj3PzPJch5cR5z49gFtzABPgJ_FVjCXM/edit?usp=sharing&pli=1&authuser=0'
+            alt='스노로즈 회계 보고 링크 바로가기'
+          >
+            스노로즈 회계 보고 링크 바로가기
+          </a>
+        </strong>
+        <br />- 자세한 내용은{' '}
+        <strong>
+          <a
+            className={styles.link}
+            href='https://www.snorose.com/board/notice/post/1840783'
+            alt='공지'
+          >
+            관련 공지
+          </a>
+        </strong>
+        를 참고해주시길 바랍니다.
+        <img src={accounting} alt='7월 회계보고' />
+      </Content>
+
+      {/* <Heading>1. 8월 스노로즈 캘린더</Heading>
+      <Content>
+        스노로즈에 대한 학우 여러분의 <strong>궁금증</strong>을 풀어드릴 수 있는
         이벤트를 준비했습니다.
         <br />
         평소에 스노로즈에게 궁금했던 점, 하고 싶었던 이야기, 혹은 바라는 점까지!
@@ -54,9 +85,9 @@ const content = (
           관련 공지
         </a>
         를 참고해주시길 바랍니다.
-        <br /> */}
+        <br />
         <img src={august} alt='8월 캘린더' />
-      </Content>
+      </Content> */}
       <hr />
       <Content>
         ※ 스노로즈 인스타그램(@snorose1906)에서 월별 스노로즈 일정을 쉽게 확인할
