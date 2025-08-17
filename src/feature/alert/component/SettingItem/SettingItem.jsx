3c6e8b27e67fcbb1234c7d9484edc72167a46175
haesa <xdhaesa@gmail.com>
2025-08-18 01:26:05 +0900

    #1108 [UI] SettingItem 설계 수정
    상태 외부로 분리, type 세분화 -> variant 재정의 및 disabled 추가

--- src/feature/alert/component/SettingItem/SettingItem.jsx
+++ src/feature/alert/component/SettingItem/SettingItem.jsx
@@ -1,43 +1,40 @@
-import { useState } from 'react';
-
 import style from './SettingItem.module.css';
 
 const backgroundStyle = {
   default: style.gradient,
   blue: style.blue,
-  grey: style.grey,
 };
 
 export default function SettingItem({
   title,
   content,
-  enabled,
-  type = 'default',
+  isEnabled,
+  onToggle,
+  variant = 'default',
+  disabled = false,
 }) {
   return (
-    <div className={`${style.container} ${backgroundStyle[type]}`}>
+    <div
+      className={`${style.container} ${backgroundStyle[variant]} ${disabled ? style.disabled : ''}`}
+    >
       <div className={style.left}>
         <div className={style.title}>{title}</div>
         <div className={style.content}>{content}</div>
       </div>
 
       <div className={style.right}>
-        <Switch enabled={enabled} />
+        <Switch isEnabled={isEnabled} onToggle={onToggle} />
         <div className={style.termsLink}>약관</div>
       </div>
     </div>
   );
 }
 
-function Switch({ enabled = false }) {
-  const [isEnabled, setIsEnabled] = useState(enabled);
-
-  const toggle = () => setIsEnabled((prev) => !prev);
-
+function Switch({ isEnabled = false, onToggle }) {
   return (
     <div
       className={`${style.switch} ${isEnabled ? style.on : style.off}`}
-      onClick={toggle}
+      onClick={onToggle}
     >
       <div className={style.slider}></div>
     </div>
