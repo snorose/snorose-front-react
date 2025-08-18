a490c8d4285dd59058c009e63bf33a3202e3c1f5
haesa <xdhaesa@gmail.com>
2025-08-19 06:33:38 +0900

    #1108 [UI] 알림함 페이지 개발

---
+++ src/dummy/data/alert.js
@@ -0,0 +1,29 @@
+export const initialAlertData = [
+  {
+    id: 9,
+    title: '등급 변경 알림',
+    body: '준회원에서 정회원(으)로 변경되었어요',
+    isRead: false,
+    createdAt: '2025-07-28T10:20:23.429Z',
+    url: '/',
+    filter: 'ETC',
+  },
+  {
+    id: 4,
+    title: '베숙트 등재 알림',
+    body: '회원님의 글이 베숙트에 올라가 5포인트를 받았어요!',
+    isRead: false,
+    createdAt: '2025-07-26T02:29:50.764Z',
+    url: '/',
+    filter: 'COMMUNITY',
+  },
+  {
+    id: 2,
+    title: '새로운 댓글이 달렸어요',
+    body: '댓글 내용',
+    isRead: true,
+    createdAt: '2025-07-26T01:29:50.764Z',
+    url: '/board/large-snow/post/1213',
+    filter: 'COMMUNITY',
+  },
+];
