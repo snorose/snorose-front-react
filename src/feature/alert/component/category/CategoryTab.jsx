50bd12b8506d5bfe27e10d77e5b61941acaf4de9
haesa <xdhaesa@gmail.com>
2025-08-19 06:22:48 +0900

    #1108 [UI] CategoryTab 개발

--- src/feature/alert/component/category/CategoryTab.jsx
+++ src/feature/alert/component/category/CategoryTab.jsx
@@ -1,5 +1,11 @@
 import style from './CategoryTab.module.css';
 
-export default function CategoryTab({ name }) {
-  return <button className={style.tabButton}>{name}</button>;
+export default function CategoryTab({ label, isSelected = false, onClick }) {
+  const selectedStyle = isSelected ? style.selected : style.unselected;
+
+  return (
+    <div className={`${style.tab} ${selectedStyle}`} onClick={onClick}>
+      {label}
+    </div>
+  );
 }
