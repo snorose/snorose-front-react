4ea3d715750fae206458883f5f29c515b5932bbb
misung-dev <misung.dev@gmail.com>
2025-09-01 23:45:18 +0900

    #1003 [SNO-84] 드롭다운 컴포넌트 디자인 변경 (#1077)
    * #1003 [STYLE] 드롭다운 컴포넌트 UI 변경
    
    * #1003 [STYLE] check 아이콘 기본 크기 변경
    
    * #1003 [STYLE] padding값 일부 수정
    
    * #1003 [FIX] 드롭다운 열림 시 레이아웃 밀림 현상 해결
    
    * #1003 [STYLE] 스크롤바 추가
    
    * #1003 [STYLE] padding값 일부 수정
    
    * #1003 [FIX] z-index값 수정으로 드롭다운과 모달창 겹침 이슈 해결
    
    * #1003 [STYLE] 회원가입시 전공 항목에 드롭다운 컴포넌트 적용
    
    * #1003 [RENAME] shared 컴포넌트 폴더로 이동 및 적용
    
    * #1003 [STYLE] rem 단위로 변경
    
    * #1003 [REMOVE] 주석 제거
    
    * #1003 [STYLE] 인라인을 외부스타일로 변경
    
    * #1003 [REMOVE] console.log 제거
    
    * #1003 [STYLE] 드롭다운 아이템 리스트에 hover 효과 적용
    
    * #1003 [FIX] 숫자로 표기하던 role ID를 상수로 변경

---
+++ src/shared/component/dropdown/SelectedItem/SelectedItem.jsx
@@ -0,0 +1,30 @@
+import { Icon } from '@/shared/component';
+
+import styles from './SelectedItem.module.css';
+
+export default function SelectedItem({ select, placeholder, isOpen, onClick }) {
+  return (
+    <div
+      className={`
+        ${styles.selectedItem}
+        ${isOpen ? styles.open : ''}
+        ${!isOpen && select?.name ? styles.colored : ''}
+      `}
+    >
+      <div
+        className={`${styles.select} ${select ? '' : styles.unselect}`}
+        onClick={onClick}
+      >
+        {select?.name ?? (
+          <span className={styles.placeholder}>{placeholder}</span>
+        )}
+        <Icon
+          id='arrow-down'
+          width={16}
+          height={9}
+          className={`${styles.arrow} ${isOpen ? styles.rotated : ''}`}
+        />
+      </div>
+    </div>
+  );
+}
