0ffe3f0f32f4a2316893b339b8b181412e72a805
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-09-05 22:43:07 +0900

    #1010 [FIX] 댓글 더보기 모달 닫혔을 때 resetCommentState

--- src/shared/component/Modal/MoreOptionModal/MoreOptionModal.jsx
+++ src/shared/component/Modal/MoreOptionModal/MoreOptionModal.jsx
@@ -2,10 +2,12 @@ import { useContext } from 'react';
 import { createPortal } from 'react-dom';
 import { Icon } from '@/shared/component';
 import { ModalContext } from '@/shared/context/ModalContext';
+import { useCommentContext } from '@/feature/comment/context';
 import styles from './MoreOptionModal.module.css';
 
 export default function MoreOptionModal({ title, optionList, functions, top }) {
   const { setModal } = useContext(ModalContext);
+  const { resetCommentState } = useCommentContext();
 
   const handleOptionClick = (item, idx) => {
     if (item.modalId) {
@@ -17,11 +19,13 @@ export default function MoreOptionModal({ title, optionList, functions, top }) {
     }
   };
 
+  const handleOutsideClick = () => {
+    setModal({ id: null, type: null });
+    resetCommentState();
+  };
+
   return createPortal(
-    <div
-      className={styles.background}
-      onClick={() => setModal({ id: null, type: null })}
-    >
+    <div className={styles.background} onClick={handleOutsideClick}>
       <div
         className={styles.container}
         onClick={(e) => e.stopPropagation()}
