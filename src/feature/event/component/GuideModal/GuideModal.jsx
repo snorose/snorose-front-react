c8451e7b11012cce8eed567e0e33eb32727be8b6
becherryu <81617058+becherryu@users.noreply.github.com>
2025-09-13 21:07:51 +0900

    #971 [REFACTOR] 가이드 모달 이미지 변경 및 멘트 수정

--- src/feature/event/component/GuideModal/GuideModal.jsx
+++ src/feature/event/component/GuideModal/GuideModal.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import styles from './GuideModal.module.css';
 import {
   noParticipationIllustration,
@@ -10,29 +12,41 @@ export default function GuideModal({ onConfirm, onClose }) {
   const [step, setStep] = useState(0);
   const guideSteps = [
     {
-      title: '무단 불참은 삼가주세요!',
-      content:
-        '무단 불참 시 경고 1회가 부여되며 향후 모든 이벤트에 참여가 불가해요.',
+      title: '무단 불참 절대 금지!',
+      content: (
+        <>
+          무단 불참 시 경고 1회가 부여되며,
+          <br />
+          향후 모든 이벤트에 참여가 불가해요
+        </>
+      ),
       image: noParticipationIllustration,
     },
     {
       title: '양도, 대리수령 절대 금지!',
-      content:
-        '양도 혹은 대리수령 시 경고 1회가 부여되며 향후 모든 이벤트에 참여가 불가해요.',
+      content: (
+        <>
+          양도,대리수령 적발 시 2년 강등되며,
+          <br />
+          향후 모든 이벤트에 참여가 불가해요
+        </>
+      ),
       image: noTransferIllustration,
     },
     {
       title: '입장권 금전 거래 절대 금지!',
       content: (
         <>
-          입장권 금전 거래 적발 시<br />
-          영구 강등 처리돼요.
+          입장권 금전 거래 적발 시,
+          <br />
+          영구 강등 처리돼요
         </>
       ),
       image: noSellIllustration,
     },
   ];
   const isLast = step === guideSteps.length - 1;
+  const navigate = useNavigate();
 
   const handleNext = () => {
     if (isLast) {
@@ -40,6 +54,10 @@ export default function GuideModal({ onConfirm, onClose }) {
     } else {
       setStep((prev) => prev + 1);
     }
+  };
+
+  const goEventNotice = () => {
+    navigate('/board/event/notice');
   };
 
   return (
@@ -70,7 +88,9 @@ export default function GuideModal({ onConfirm, onClose }) {
           ))}
         </div>
         <div className={styles.buttons}>
-          <button className={styles.goNotice}>공지 보기</button>
+          <button className={styles.goNotice} onClick={goEventNotice}>
+            공지 보기
+          </button>
           <button className={styles.confirm} onClick={handleNext}>
             {isLast ? '신청하기' : '다음'}
           </button>
