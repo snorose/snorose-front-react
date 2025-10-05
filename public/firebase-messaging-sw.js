a00fefd79ff7238f42120d18609d57b086266a5c
haesa <xdhaesa@gmail.com>
2025-10-05 16:59:51 +0900

    #1211 [FIX] 안드로이드에서 푸시 알림이 숨는 문제 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,4 +15,40 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging(app);
 
-messaging.onBackgroundMessage((payload) => {});
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message:',
+    payload
+  );
+
+  const { title, body, url } = payload.data;
+
+  const notificationOptions = {
+    body,
+    data: { url },
+  };
+
+  self.registration.showNotification(title, notificationOptions);
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  const targetUrl = event.notification.data?.url || '/';
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then((clientList) => {
+        // 이미 열린 창이 있으면 focus
+        for (const client of clientList) {
+          if (client.url.includes(targetUrl) && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        // 없으면 새 탭 열기
+        if (clients.openWindow) {
+          return clients.openWindow(targetUrl);
+        }
+      })
+  );
+});
