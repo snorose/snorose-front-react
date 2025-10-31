8ca0aeaefb21180be48f813c9e68fa37012a2945
becherryu <81617058+becherryu@users.noreply.github.com>
2025-10-31 18:34:20 +0900

    #971 [REFACTOR] 안내 모달 저장 위치 변경 및 constant로 수정

--- src/shared/constant/modalOptions.js
+++ src/shared/constant/modalOptions.js
@@ -3,11 +3,6 @@ import {
   POST_REPORT_TYPE_MODAL_OPTION_CHILDREN,
   USER_REPORT_TYPE_MODAL_OPTION_CHILDREN,
 } from './board';
-import {
-  noParticipationIllustration,
-  noSellIllustration,
-  noTransferIllustration,
-} from '@/assets/illustrations';
 
 export const MODAL_OPTIONS = [
   {
@@ -239,39 +234,3 @@ export const MODAL_CONFIRM = {
     ),
   },
 };
-
-export const EVENT_GUIDE_MODAL_OPTIONS = [
-  {
-    title: '무단 불참 절대 금지!',
-    content: (
-      <>
-        무단 불참 시 경고 1회가 부여되며,
-        <br />
-        향후 모든 이벤트에 참여가 불가해요
-      </>
-    ),
-    image: noParticipationIllustration,
-  },
-  {
-    title: '양도, 대리수령 절대 금지!',
-    content: (
-      <>
-        양도,대리수령 적발 시 2년 강등되며,
-        <br />
-        향후 모든 이벤트에 참여가 불가해요
-      </>
-    ),
-    image: noTransferIllustration,
-  },
-  {
-    title: '입장권 금전 거래 절대 금지!',
-    content: (
-      <>
-        입장권 금전 거래 적발 시,
-        <br />
-        영구 강등 처리돼요
-      </>
-    ),
-    image: noSellIllustration,
-  },
-];
