d07ae3512210195ecc968ba83f12f15a901349f6
haesa <xdhaesa@gmail.com>
2025-11-11 04:12:28 +0900

    #1290 [REFACTOR] Input 컴포넌트 교체 - 로그인

--- src/page/account/LoginPage/LoginPage.jsx
+++ src/page/account/LoginPage/LoginPage.jsx
@@ -3,7 +3,13 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { useLogin } from '@/apis';
 
-import { BackAppBar, Button, Icon, Input } from '@/shared/component';
+import {
+  BackAppBar,
+  Icon,
+  NewButton,
+  PasswordInput,
+  TextInput,
+} from '@/shared/component';
 
 import snoroseLogo from '@/assets/images/snoroseLogo.svg';
 
@@ -14,12 +20,8 @@ export default function Login() {
   const login = useLogin();
   const [formData, setFormData] = useState({ loginId: '', password: '' });
   const [isError, setIsError] = useState(false);
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setIsPasswordVisible((prev) => !prev);
-  };
+  const [isChecked, setIsChecked] = useState(false);
 
   const toggleRememberId = () => {
     setIsChecked((prev) => !prev);
@@ -44,83 +46,66 @@ export default function Login() {
     }
   }, []);
 
+  const inputProps = [
+    {
+      type: 'text',
+      id: 'loginId',
+      placeholder: '아이디',
+      value: formData.loginId,
+      onChange: (next) => setFormData((prev) => ({ ...prev, loginId: next })),
+    },
+    {
+      type: 'password',
+      id: 'password',
+      placeholder: '영어, 숫자, 특수문자를 포함한 비밀번호',
+      value: formData.password,
+      onChange: (next) => setFormData((prev) => ({ ...prev, password: next })),
+    },
+  ];
+
   return (
-    <div className={styles.loginframe}>
-      <BackAppBar notFixed />
+    <div className={styles.container}>
+      <BackAppBar />
 
       <form onSubmit={handleLoginSubmit}>
-        <div className={styles.loginBody}>
-          <img src={snoroseLogo} alt='스노로즈 로고' className={styles.logo} />
-          <p className={styles.title}>
-            숙명인을 위한 커뮤니티,
-            <br />
-            스노로즈에 오신 것을 환영합니다!
-          </p>
+        <img src={snoroseLogo} alt='스노로즈 로고' className={styles.logo} />
 
-          <div className={styles.inputWrapper}>
-            <div
-              className={styles.input}
-              onChange={() => {
-                setIsError(false);
-              }}
-            >
-              <Input
-                placeholder={'아이디'}
-                className={isError ? 'wrong' : 'ready'}
-                inputType={'loginId'}
-                inputData={setFormData}
-                value={formData.loginId}
-              />
-            </div>
-            <div
-              className={!isError ? styles.input : undefined}
-              onChange={() => {
-                setIsError(false);
-              }}
-            >
-              <div
-                className={`${styles.pwFrame} ${styles[isError ? 'wrong' : 'ready']}`}
-              >
-                <Input
-                  type={isPasswordVisible ? 'text' : 'password'}
-                  placeholder={'영어, 숫자, 특수문자를 포함한 비밀번호'}
-                  className={isError ? 'wrong' : 'ready'}
-                  inputType={'password'}
-                  inputData={setFormData}
-                  value={formData.password}
-                />
-                {formData.password && (
-                  <Icon
-                    id={isPasswordVisible ? 'closed-eye' : 'opened-eye'}
-                    fill={isError ? '#ff4b6c' : '#898989'}
-                    width={24}
-                    height={24}
-                    className={styles.visibility}
-                    onClick={togglePasswordVisibility}
-                  />
-                )}
-              </div>
-            </div>
-          </div>
+        <p className={styles.title}>
+          숙명인을 위한 커뮤니티,
+          <br />
+          스노로즈에 오신 것을 환영합니다!
+        </p>
 
-          <div className={styles.RememberIdCheckbox} onClick={toggleRememberId}>
-            <Icon
-              id={isChecked ? 'inactive-check-circle' : 'active-check-circle'}
-              width={22}
-              height={22}
-            />
-            <span>아이디 기억하기</span>
-          </div>
+        <div className={styles.form}>
+          {inputProps.map((props) => {
+            const Input = {
+              text: TextInput,
+              password: PasswordInput,
+            }[props.type];
 
-          <Button btnName='로그인하기' className='right' />
+            let status = isError ? 'error' : 'default';
+            status = props.value === '' ? 'default' : status;
+            return <Input status={status} {...props} />;
+          })}
+        </div>
 
-          <div className={styles.find}>
-            <Link to='/signup'>회원가입하기</Link>
-            <p className={styles.divider}>|</p>
-            <Link to='/find-id'>아이디 찾기</Link>
-            <p className={styles.divider}>|</p>
-            <Link to='/find-pw'>비밀번호 찾기</Link>
-          </div>
+        <div className={styles.rememberIdCheckbox} onClick={toggleRememberId}>
+          <Icon
+            id={isChecked ? 'inactive-check-circle' : 'active-check-circle'}
+            width={22}
+            height={22}
+          />
+          <span>아이디 기억하기</span>
+        </div>
+
+        <NewButton>로그인하기</NewButton>
+
+        <div className={styles.find}>
+          <Link to='/signup'>회원가입하기</Link>
+          <p className={styles.divider}>|</p>
+          <Link to='/find-id'>아이디 찾기</Link>
+          <p className={styles.divider}>|</p>
+          <Link to='/find-pw'>비밀번호 찾기</Link>
         </div>
       </form>
     </div>
