167aab7add5e840c29e4e03d0810bc8aa4271c1b
becherryu <81617058+becherryu@users.noreply.github.com>
2025-11-10 18:25:41 +0900

    #1272 [REFACTROR] background CSS 모듈 클래스로 수정

--- src/feature/home/component/HomeBoardCard/HomeBoardCard.jsx
+++ src/feature/home/component/HomeBoardCard/HomeBoardCard.jsx
@@ -7,27 +7,21 @@ import lockImage from '@/assets/images/lock.svg';
 
 import styles from './HomeBoardCard.module.css';
 
-export default function HomeBoardCard({ className, path, name, mainImage }) {
+export default function HomeBoardCard({ path, name, mainImage }) {
   const { status } = useAuth();
   const isLogin = status === USER_STATUS.isLogin;
 
-  const backgroundColors = {
-    첫눈온방: 'var(--firstSnow-bg)',
-    함박눈방: 'var(--largeSnow-bg)',
-    만년설방: 'var(--permanentSnow-bg)',
-    이벤트: 'var(--event-bg)',
+  const backgroundClass = {
+    첫눈온방: styles.firstSnow,
+    함박눈방: styles.largeSnow,
+    만년설방: styles.permanentSnow,
+    이벤트: styles.event,
   };
-  const bgColor = backgroundColors[name] || 'var(--grey-1)';
 
   return (
-    <Link className={`${styles.link} ${className}`} to={path}>
+    <Link className={`${styles.link}`} to={path}>
       <div className={styles.cardSection}>
-        <div
-          className={styles.card}
-          style={{
-            background: bgColor,
-          }}
-        >
+        <div className={`${styles.card} ${backgroundClass[name]}`}>
           <img
             src={isLogin ? mainImage : lockImage}
             alt={name}
