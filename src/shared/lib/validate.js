b4db1c5e44c6173451acfda33e2dc25192973c37
haesa <34948133+haesa@users.noreply.github.com>
2025-11-24 17:26:40 +0900

    #1303 [SNO-223, SNO-240] Input 컴포넌트 교체 - 숙명인 인증 (#1306)
    * #1276 [REMOVE] 사용하지 않는 컴포넌트 삭제
    
    * #1267 [REFACTOR] 숙명인 인증 - Input 컴포넌트 교체
    
    * #1267 [REMOVE] BackAppBar 불필요한 CSS 삭제
    
    * #1267 [MODIFY] 뷰포트 너비 380px 이하에서 font-size 55%로 변경

--- src/shared/lib/validate.js
+++ src/shared/lib/validate.js
@@ -1,7 +1,3 @@
-export const isEmailValid = (email) => {
-  return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
-};
-
 export const isNumber = (value) => {
   return /^\d+$/.test(value);
 };
