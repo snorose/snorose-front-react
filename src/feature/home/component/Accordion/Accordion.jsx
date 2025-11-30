1adae8e4391b63df62eae5ceb98a6b5856d631b8
becherryu <81617058+becherryu@users.noreply.github.com>
2025-11-30 21:21:06 +0900

    #1318 [MODIFY] about 스노로즈 내 리자 모집 문구 변경

--- src/feature/home/component/Accordion/Accordion.jsx
+++ src/feature/home/component/Accordion/Accordion.jsx
@@ -2,12 +2,18 @@ import { Icon, PrimaryButton } from '@/shared/component';
 
 import styles from './Accordion.module.css';
 
+const RECRUIT_START = new Date('2025-12-01T00:00:00+09:00');
+const RECRUIT_END = new Date('2025-12-28T00:00:00+09:00');
+
 const goApply = () => {
   window.location.href =
-    'https://www.notion.so/snorose/2024-2-10c7ef0aa3bf8027a04ee35b7c521e12';
+    'https://www.notion.so/snorose/10c7ef0aa3bf8027a04ee35b7c521e12';
 };
 
 export default function Accordion({ title, children, isOpen, onClick }) {
+  const now = new Date();
+  const isRecruit = now >= RECRUIT_START && now <= RECRUIT_END;
+
   return (
     <div className={`${styles.accordion} ${isOpen && styles.opend}`}>
       <div className={`${styles.panelHeader}`} onClick={onClick}>
@@ -21,12 +27,8 @@ export default function Accordion({ title, children, isOpen, onClick }) {
       </div>
       <div className={`${styles.panelBody}`}>{children}</div>
       {title === '명예의 전당' && isOpen && (
-        <PrimaryButton
-          className={styles.apply}
-          // onClick={() => alert('지원 기간이 아닙니다!')}
-          onClick={goApply}
-        >
-          지난 모집 공고 확인하기
+        <PrimaryButton className={styles.apply} onClick={goApply}>
+          {isRecruit ? '리자 지원하러 가기' : '지난 모집 공고 확인하기'}
         </PrimaryButton>
       )}
     </div>
