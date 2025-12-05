f848a8b937780350b98a588c050981d18d422c4c
haesa <xdhaesa@gmail.com>
2025-12-05 12:33:32 +0900

    #1329 [REFACTOR] 신고글 작성 페이지의 reportType의 타입 안정성 확보

--- src/page/support/WriteReportPage/WriteReportPage.tsx
+++ src/page/support/WriteReportPage/WriteReportPage.tsx
@@ -2,12 +2,17 @@ import { useParams } from 'react-router-dom';
 
 import { NotFoundPage } from '@/page/etc';
 
-const VALID_REPORT_TYPES = ['post', 'comment', 'user', 'exam'];
+const VALID_REPORT_TYPES = ['post', 'comment', 'user', 'exam'] as const;
+type ReportType = (typeof VALID_REPORT_TYPES)[number];
+
+function isValidReportType(x: any): x is ReportType {
+  return VALID_REPORT_TYPES.includes(x);
+}
 
 export default function WriteReportPage() {
   const { reportType } = useParams();
 
-  if (!VALID_REPORT_TYPES.includes(reportType)) {
+  if (!isValidReportType(reportType)) {
     return <NotFoundPage />;
   }
 
